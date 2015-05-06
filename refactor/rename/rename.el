211e78d7bada366a6f48bcf2c03f7ffb9a2a8b61
Jeff Hodges <jeff@somethingsimilar.com>
2015-05-06 13:02:21 -0700

    add 'Or' to build comment
    go get does the install work for you.

--- refactor/rename/rename.el
+++ refactor/rename/rename.el
@@ -6,6 +6,7 @@
 ;;;
 ;;; To install:
 ;;; % go get golang.org/x/tools/cmd/gorename
+;;; Or
 ;;; % go build golang.org/x/tools/cmd/gorename
 ;;; % mv gorename $HOME/bin/         # or elsewhere on $PATH
 ;;;
