223bd61b40f11451f42222cf6cf831ac62976ecc
lucor <lu.corbo@gmail.com>
2018-03-21 08:59:11 +0100

    tools/present: add support for quote
    The template uses the function "quote" to inject quotes and citations.
    
    The text after ".quote" is embedded in a blockquote element after
    processing styling. The text after the optional "//CITATION:" is treated
    as a citation and embedded in a cite element after processing styling.
    
    	.quote Never memorize something that you can look up //CITATION: Albert Einstein
    
    Change-Id: I891285928410cea1d289f05b9b5e8599ffaa79b6

---
+++ present/quote.go
@@ -0,0 +1,50 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package present
+
+import (
+	"fmt"
+	"strings"
+)
+
+func init() {
+	Register("quote", parseQuote)
+}
+
+type Quote struct {
+	Text     string
+	Citation string
+}
+
+func (c Quote) TemplateName() string { return "quote" }
+
+const citationToken = "//CITATION:"
+
+// parseQuote parses a quote present directive. Its syntax:
+//   .quote <text> [citation]
+func parseQuote(_ *Context, sourceFile string, sourceLine int, cmd string) (Elem, error) {
+
+	cmd = strings.TrimSpace(strings.TrimPrefix(cmd, ".quote"))
+
+	tokens := strings.Split(cmd, citationToken)
+
+	text := strings.TrimSpace(strings.TrimPrefix(tokens[0], ".quote"))
+	citation := ""
+
+	if text == "" {
+		return nil, fmt.Errorf("%s:%d invalid quote syntax", sourceFile, sourceLine)
+	}
+
+	if len(tokens) == 1 {
+		return Quote{text, citation}, nil
+	}
+
+	citation = strings.TrimSpace(tokens[1])
+	if citation == "" || len(tokens) > 2 {
+		return nil, fmt.Errorf("%s:%d invalid citation syntax", sourceFile, sourceLine)
+	}
+
+	return Quote{text, citation}, nil
+}
