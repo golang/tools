34e552fd69fb785601c9b59dc1a179228aebabbb
zakhar.malinovskiy <zakhar.malinovskiy@gmail.com>
2019-11-13 12:16:12 +0200

    godoc/static: This change adds css style to a tooltip that popups on links.
    The change makes bigger tooltip on every link to type, var, func on doc page.
    when godoc is run with -analysis=type the default tooltip is too small.
    Originated from https://stackoverflow.com/a/16462668

--- godoc/static/godocs.js
+++ godoc/static/godocs.js
@@ -10,6 +10,28 @@
  *  + Bind links to foldable sections (bindToggleLinks)
  */
 
+(function () {
+    var ID = "tooltip", CLS_ON = "tooltip_ON", FOLLOW = true,
+    DATA = "_tooltip", OFFSET_X = 20, OFFSET_Y = 10,
+    showAt = function (e) {
+        var ntop = e.pageY + OFFSET_Y, nleft = e.pageX + OFFSET_X;
+        $("#" + ID).html($(e.target).data(DATA)).css({
+            position: "absolute", top: ntop, left: nleft
+        }).show();
+    };
+    $(document).on("mouseenter", "*[title]", function (e) {
+        $(this).data(DATA, $(this).attr("title"));
+        $(this).removeAttr("title").addClass(CLS_ON);
+        $("<div id='" + ID + "' />").appendTo("body");
+        showAt(e);
+    });
+    $(document).on("mouseleave", "." + CLS_ON, function (e) {
+        $(this).attr("title", $(this).data(DATA)).removeClass(CLS_ON);
+        $("#" + ID).remove();
+    });
+    if (FOLLOW) { $(document).on("mousemove", "." + CLS_ON, showAt); }
+}());
+
 (function() {
 'use strict';
 
