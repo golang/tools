golang/tools

commit c5061a5744588807f2f4338273fa23abd9bcdf74

internal/lsp: reorganize the generated Go code for the lsp protocol
Code generation has been unified, so that tsprotocol.go and tsserver.go
are produced by the same program. tsprotocol.go is about 900 lines shorter,
partly from removing boilerplate comments that golint no longer requires.
(And partly by generating fewer unneeded types.)

The choice made for a union type is commented with the set of types. There
is no Go equivalent for union types, but making themn all interface{}
would replace type checking at unmarshalling with checking runtime
conversions.

Intersection types (A&B) are sometimes embedded (struct{A;B;}, and
sometimes expanded, as they have to be if A and B have fields with the
same names.

There are fewer embedded structs, which had been verbose and confusing to
initialize. They have been replaced by types whose names end in Gn.

Essentially all the generated *structs have been removed. This makes
no difference in what the client sends, and the server may send a {}
where it previously might have sent nothing. The benefit is that some
nil tests can be removed. Thus 'omitempty' in json tags is just
documentation that the element is optional in the protocol.

The files that generate this code will be submitted later, but soon.

Change-Id: I52b997d9c58de3d733fc8c6ce061e47ce2bdb100
Reviewed-on: https://go-review.googlesource.com/c/tools/+/207598
Run-TryBot: Peter Weinberger <pjw@google.com>
TryBot-Result: Gobot Gobot <gobot@golang.org>
Reviewed-by: Ian Cottrell <iancottrell@google.com>

internal/lsp/text_synchronization.go
@@ -96,7 +96,7 @@ func fullChange(changes []protocol.TextDocumentContentChangeEvent) (string, bool
 		return "", false
 	}
 	// The length of the changes must be 1 at this point.
-	if changes[0].Range == nil && changes[0].RangeLength == 0 {
+	if changes[0].RangeLength == 0 { // used to check changes[0].Range == nil
 		return changes[0].Text, true
 	}
 	return "", false
@@ -116,7 +116,7 @@ func (s *Server) applyChanges(ctx context.Context, uri span.URI, changes []proto
 			Content:   content,
 		}
 
-		spn, err := m.RangeSpan(*change.Range)
+		spn, err := m.RangeSpan(change.Range)
 		if err != nil {
 			return "", err
 		}

internal/lsp/cmd/cmd.go
@@ -204,10 +204,10 @@ func (app *Application) connect(ctx context.Context) (*connection, error) {
 }
 
 func (c *connection) initialize(ctx context.Context) error {
-	params := &protocol.ParamInitia{}
+	params := &protocol.ParamInitialize{}
 	params.RootURI = string(span.FileURI(c.Client.app.wd))
 	params.Capabilities.Workspace.Configuration = true
-	params.Capabilities.TextDocument.Hover = &protocol.HoverClientCapabilities{
+	params.Capabilities.TextDocument.Hover = protocol.HoverClientCapabilities{
 		ContentFormat: []protocol.MarkupKind{protocol.PlainText},
 	}
 	if _, err := c.Server.Initialize(ctx, params); err != nil {
@@ -295,7 +295,7 @@ func (c *cmdClient) WorkspaceFolders(ctx context.Context) ([]protocol.WorkspaceF
 	return nil, nil
 }
 
-func (c *cmdClient) Configuration(ctx context.Context, p *protocol.ParamConfig) ([]interface{}, error) {
+func (c *cmdClient) Configuration(ctx context.Context, p *protocol.ParamConfiguration) ([]interface{}, error) {
 	results := make([]interface{}, len(p.Items))
 	for i, item := range p.Items {
 		if item.Section != "gopls" {

internal/lsp/cmd/imports.go
@@ -69,7 +69,11 @@ func (t *imports) Run(ctx context.Context, args ...string) error {
 		return errors.Errorf("%v: %v", from, err)
 	}
 	var edits []protocol.TextEdit
-	for _, a := range actions {
+	v, ok := actions.([]protocol.CodeAction)
+	if !ok {
+		return errors.Errorf("expected CodeAction, got %T", actions)
+	}
+	for _, a := range v {
 		if a.Title != "Organize Imports" {
 			continue
 		}

internal/lsp/cmd/suggested_fix.go
@@ -87,7 +87,11 @@ func (s *suggestedfix) Run(ctx context.Context, args ...string) error {
 		return errors.Errorf("%v: %v", from, err)
 	}
 	var edits []protocol.TextEdit
-	for _, a := range actions {
+	v, ok := actions.([]protocol.CodeAction)
+	if !ok {
+		return errors.Errorf("expected CodeAction, got %T", actions)
+	}
+	for _, a := range v {
 		if !a.IsPreferred && !s.All {
 			continue
 		}

internal/lsp/cmd/symbols.go
@@ -52,7 +52,6 @@ func (r *symbols) Run(ctx context.Context, args ...string) error {
 	if err != nil {
 		return err
 	}
-
 	for _, s := range symbols {
 		fmt.Println(symbolToString(s))
 		// Sort children for consistency

internal/lsp/code_action.go
@@ -63,7 +63,7 @@ func (s *Server) codeAction(ctx context.Context, params *protocol.CodeActionPara
 		codeActions = append(codeActions, protocol.CodeAction{
 			Title: "Tidy",
 			Kind:  protocol.SourceOrganizeImports,
-			Command: &protocol.Command{
+			Command: protocol.Command{
 				Title:   "Tidy",
 				Command: "tidy",
 				Arguments: []interface{}{
@@ -94,7 +94,7 @@ func (s *Server) codeAction(ctx context.Context, params *protocol.CodeActionPara
 						codeActions = append(codeActions, protocol.CodeAction{
 							Title: importFixTitle(importFix.Fix),
 							Kind:  protocol.QuickFix,
-							Edit: &protocol.WorkspaceEdit{
+							Edit: protocol.WorkspaceEdit{
 								DocumentChanges: documentChanges(fh, importFix.Edits),
 							},
 							Diagnostics: fixDiagnostics,
@@ -107,7 +107,7 @@ func (s *Server) codeAction(ctx context.Context, params *protocol.CodeActionPara
 			codeActions = append(codeActions, protocol.CodeAction{
 				Title: "Organize Imports",
 				Kind:  protocol.SourceOrganizeImports,
-				Edit: &protocol.WorkspaceEdit{
+				Edit: protocol.WorkspaceEdit{
 					DocumentChanges: documentChanges(fh, edits),
 				},
 			})
@@ -226,7 +226,7 @@ func quickFixes(ctx context.Context, s source.Snapshot, f source.File, diagnosti
 				Title:       fix.Title,
 				Kind:        protocol.QuickFix,
 				Diagnostics: []protocol.Diagnostic{diag},
-				Edit:        &protocol.WorkspaceEdit{},
+				Edit:        protocol.WorkspaceEdit{},
 			}
 			for uri, edits := range fix.Edits {
 				f, err := s.View().GetFile(ctx, uri)

internal/lsp/completion.go
@@ -104,7 +104,7 @@ func toProtocolCompletionItems(candidates []source.CompletionItem, rng protocol.
 			Label:  candidate.Label,
 			Detail: candidate.Detail,
 			Kind:   candidate.Kind,
-			TextEdit: &protocol.TextEdit{
+			TextEdit: protocol.TextEdit{
 				NewText: insertText,
 				Range:   rng,
 			},
@@ -123,7 +123,7 @@ func toProtocolCompletionItems(candidates []source.CompletionItem, rng protocol.
 		// since we show return types as well.
 		switch item.Kind {
 		case protocol.FunctionCompletion, protocol.MethodCompletion:
-			item.Command = &protocol.Command{
+			item.Command = protocol.Command{
 				Command: "editor.action.triggerParameterHints",
 			}
 		}

internal/lsp/general.go
@@ -20,7 +20,7 @@ import (
 	errors "golang.org/x/xerrors"
 )
 
-func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitia) (*protocol.InitializeResult, error) {
+func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitialize) (*protocol.InitializeResult, error) {
 	s.stateMu.Lock()
 	state := s.state
 	s.stateMu.Unlock()
@@ -53,8 +53,7 @@ func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitia) (
 	}
 
 	var codeActionProvider interface{}
-	if ca := params.Capabilities.TextDocument.CodeAction; ca != nil && ca.CodeActionLiteralSupport != nil &&
-		len(ca.CodeActionLiteralSupport.CodeActionKind.ValueSet) > 0 {
+	if ca := params.Capabilities.TextDocument.CodeAction; len(ca.CodeActionLiteralSupport.CodeActionKind.ValueSet) > 0 {
 		// If the client has specified CodeActionLiteralSupport,
 		// send the code actions we support.
 		//
@@ -65,18 +64,16 @@ func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitia) (
 	} else {
 		codeActionProvider = true
 	}
-	var renameOpts interface{}
-	if r := params.Capabilities.TextDocument.Rename; r != nil {
-		renameOpts = &protocol.RenameOptions{
-			PrepareProvider: r.PrepareSupport,
-		}
-	} else {
-		renameOpts = true
+	// This used to be interface{}, when r could be nil
+	var renameOpts protocol.RenameOptions
+	r := params.Capabilities.TextDocument.Rename
+	renameOpts = protocol.RenameOptions{
+		PrepareProvider: r.PrepareSupport,
 	}
 	return &protocol.InitializeResult{
 		Capabilities: protocol.ServerCapabilities{
 			CodeActionProvider: codeActionProvider,
-			CompletionProvider: &protocol.CompletionOptions{
+			CompletionProvider: protocol.CompletionOptions{
 				TriggerCharacters: []string{"."},
 			},
 			DefinitionProvider:         true,
@@ -84,35 +81,27 @@ func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitia) (
 			ImplementationProvider:     true,
 			DocumentFormattingProvider: true,
 			DocumentSymbolProvider:     true,
-			ExecuteCommandProvider: &protocol.ExecuteCommandOptions{
+			ExecuteCommandProvider: protocol.ExecuteCommandOptions{
 				Commands: options.SupportedCommands,
 			},
 			FoldingRangeProvider:      true,
 			HoverProvider:             true,
 			DocumentHighlightProvider: true,
-			DocumentLinkProvider:      &protocol.DocumentLinkOptions{},
+			DocumentLinkProvider:      protocol.DocumentLinkOptions{},
 			ReferencesProvider:        true,
 			RenameProvider:            renameOpts,
-			SignatureHelpProvider: &protocol.SignatureHelpOptions{
+			SignatureHelpProvider: protocol.SignatureHelpOptions{
 				TriggerCharacters: []string{"(", ","},
 			},
 			TextDocumentSync: &protocol.TextDocumentSyncOptions{
 				Change:    options.TextDocumentSyncKind,
 				OpenClose: true,
-				Save: &protocol.SaveOptions{
+				Save: protocol.SaveOptions{
 					IncludeText: false,
 				},
 			},
-			Workspace: &struct {
-				WorkspaceFolders *struct {
-					Supported           bool   "json:\"supported,omitempty\""
-					ChangeNotifications string "json:\"changeNotifications,omitempty\""
-				} "json:\"workspaceFolders,omitempty\""
-			}{
-				WorkspaceFolders: &struct {
-					Supported           bool   "json:\"supported,omitempty\""
-					ChangeNotifications string "json:\"changeNotifications,omitempty\""
-				}{
+			Workspace: protocol.WorkspaceGn{
+				protocol.WorkspaceFoldersGn{
 					Supported:           true,
 					ChangeNotifications: "workspace/didChangeWorkspaceFolders",
 				},
@@ -192,7 +181,7 @@ func (s *Server) fetchConfig(ctx context.Context, name string, folder span.URI, 
 	if !s.session.Options().ConfigurationSupported {
 		return nil
 	}
-	v := protocol.ParamConfig{
+	v := protocol.ParamConfiguration{
 		ConfigurationParams: protocol.ConfigurationParams{
 			Items: []protocol.ConfigurationItem{{
 				ScopeURI: protocol.NewURI(folder),

commit 4ee1c5cab3f070d5605db1bce6d0dc34a52b3a72

internal/lsp: handle breakage caused by CL 207598
textDocument/didChange events need to indicate if the change includes
the full file content or just a diff. Previously, the
contentChange.Range field was a pointer, so if it was nil, then we would
conclude that the file change was for the whole file. Now, the best we
can do is compare it to an empty range, but this still doesn't work if
you are at the beginning of a file. I think that the range needs to be a
pointer for this to work correctly.

Also, some minor changes that came up along the way while debugging:
(1) Don't close over the *cache variable for fear of pinning anything in
    memory
(2) Improve the error message when the token.File is nil
(3) Check for a nil token.File earlier

Change-Id: If9f310e92b7fb740b45e6cd3f9ca678a6fb52ff6
Reviewed-on: https://go-review.googlesource.com/c/tools/+/207906
Reviewed-by: Heschi Kreinick <heschi@google.com>

internal/lsp/cache/parse.go
@@ -52,9 +52,10 @@ func (c *cache) ParseGoHandle(fh source.FileHandle, mode source.ParseMode) sourc
 		file: fh.Identity(),
 		mode: mode,
 	}
+	fset := c.fset
 	h := c.store.Bind(key, func(ctx context.Context) interface{} {
 		data := &parseGoData{}
-		data.ast, data.mapper, data.parseError, data.err = parseGo(ctx, c, fh, mode)
+		data.ast, data.mapper, data.parseError, data.err = parseGo(ctx, fset, fh, mode)
 		return data
 	})
 	return &parseGoHandle{
@@ -105,7 +106,7 @@ func hashParseKeys(phs []source.ParseGoHandle) string {
 	return hashContents(b.Bytes())
 }
 
-func parseGo(ctx context.Context, c *cache, fh source.FileHandle, mode source.ParseMode) (file *ast.File, mapper *protocol.ColumnMapper, parseError error, err error) {
+func parseGo(ctx context.Context, fset *token.FileSet, fh source.FileHandle, mode source.ParseMode) (file *ast.File, mapper *protocol.ColumnMapper, parseError error, err error) {
 	ctx, done := trace.StartSpan(ctx, "cache.parseGo", telemetry.File.Of(fh.Identity().URI.Filename()))
 	defer done()
 
@@ -119,18 +120,21 @@ func parseGo(ctx context.Context, c *cache, fh source.FileHandle, mode source.Pa
 	if mode == source.ParseHeader {
 		parserMode = parser.ImportsOnly | parser.ParseComments
 	}
-	file, parseError = parser.ParseFile(c.fset, fh.Identity().URI.Filename(), buf, parserMode)
+	file, parseError = parser.ParseFile(fset, fh.Identity().URI.Filename(), buf, parserMode)
+	var tok *token.File
 	if file != nil {
+		// Fix any badly parsed parts of the AST.
+		tok = fset.File(file.Pos())
+		if tok == nil {
+			return nil, nil, nil, errors.Errorf("successfully parsed but no token.File for %s (%v)", fh.Identity().URI, parseError)
+		}
 		if mode == source.ParseExported {
 			trimAST(file)
 		}
-		// Fix any badly parsed parts of the AST.
-		tok := c.fset.File(file.Pos())
 		if err := fix(ctx, file, tok, buf); err != nil {
 			log.Error(ctx, "failed to fix AST", err)
 		}
 	}
-
 	if file == nil {
 		// If the file is nil only due to parse errors,
 		// the parse errors are the actual errors.
@@ -140,10 +144,6 @@ func parseGo(ctx context.Context, c *cache, fh source.FileHandle, mode source.Pa
 		}
 		return nil, nil, parseError, err
 	}
-	tok := c.FileSet().File(file.Pos())
-	if tok == nil {
-		return nil, nil, parseError, errors.Errorf("no token.File for %s", fh.Identity().URI)
-	}
 	uri := fh.Identity().URI
 	content, _, err := fh.Read(ctx)
 	if err != nil {
@@ -151,7 +151,7 @@ func parseGo(ctx context.Context, c *cache, fh source.FileHandle, mode source.Pa
 	}
 	m := &protocol.ColumnMapper{
 		URI:       uri,
-		Converter: span.NewTokenConverter(c.FileSet(), tok),
+		Converter: span.NewTokenConverter(fset, tok),
 		Content:   content,
 	}
 	return file, m, parseError, nil

internal/lsp/text_synchronization.go
@@ -96,9 +96,11 @@ func fullChange(changes []protocol.TextDocumentContentChangeEvent) (string, bool
 		return "", false
 	}
 	// The length of the changes must be 1 at this point.
-	if changes[0].RangeLength == 0 { // used to check changes[0].Range == nil
+	// TODO: This breaks if you insert a character at the beginning of the file.
+	if (changes[0].Range == protocol.Range{} && changes[0].RangeLength == 0) {
 		return changes[0].Text, true
 	}
+
 	return "", false
 }
 