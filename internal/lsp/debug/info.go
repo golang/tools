c00a2daf0567aa36c55c736e0800c2ff763af846
Rebecca Stambler <rstambler@golang.org>
2020-01-28 17:06:52 -0500

    internal/lsp/debug: update hardcoded version string for pre-release
    Second pre-release.
    
    Change-Id: I8962f033649b8916017d84601a2646b7edb79af5
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/216719
    Reviewed-by: Robert Findley <rfindley@google.com>

--- internal/lsp/debug/info.go
+++ internal/lsp/debug/info.go
@@ -21,7 +21,7 @@ const (
 )
 
 // Version is a manually-updated mechanism for tracking versions.
-var Version = "master"
+var Version = "v0.3.0-pre2"
 
 // PrintServerInfo writes HTML debug info to w for the Instance s.
 func PrintServerInfo(w io.Writer, s Instance) {
