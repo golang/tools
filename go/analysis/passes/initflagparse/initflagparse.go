2d1136b937a65f4ef8960e34c8b37ebf4efe88e0
Jesús Espino <jespinog@gmail.com>
2020-02-09 19:24:23 +0100

    Fixing copy/paste problem

--- go/analysis/passes/initflagparse/initflagparse.go
+++ go/analysis/passes/initflagparse/initflagparse.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Package initflagparse defines an Analyzer that checks for invalid
-// conversions of uintptr to unsafe.Pointer.
+// usages of flag.Parse during package initialization
 package initflagparse
 
 import (
