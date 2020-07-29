dc9b9f800d3815a029a3a61cfc42136d201506ab
Rebecca Stambler <rstambler@golang.org>
2020-07-29 11:48:35 -0400

    [gopls-release-branch.0.4] internal/lsp: properly check for nil on i.enclosing
    Fixes golang/go#40479
    
    Change-Id: If827a86fd603574d3adde295c1165bb6267732eb
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/245486
    Run-TryBot: Rebecca Stambler <rstambler@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>
    (cherry picked from commit 449c585133f022949997079b2cc2e75636d11b2b)
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/245538
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Ian Cottrell <iancottrell@google.com>

--- internal/lsp/source/identifier.go
+++ internal/lsp/source/identifier.go
@@ -256,9 +256,6 @@ func searchForEnclosing(info *types.Info, path []ast.Node) types.Type {
 						}
 					}
 				}
-				if exported == nil {
-					return nil
-				}
 				return exported
 			}
 		case *ast.CompositeLit:
