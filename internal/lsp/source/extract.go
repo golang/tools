golang/tools

commit 86480093e99fd84a79624be4272aa3be6e209d3a

internal/lsp/source: fix nil pointer in extract function
Ran into this while debugging another issue.

Change-Id: I154493418c7676a24457a4e11431ad4f0311c07a
Reviewed-on: https://go-review.googlesource.com/c/tools/+/246757
Run-TryBot: Rebecca Stambler <rstambler@golang.org>
TryBot-Result: Gobot Gobot <gobot@golang.org>
Reviewed-by: Josh Baum <joshbaum@google.com>

internal/lsp/source/extract.go
@@ -477,8 +477,8 @@ func adjustRangeForWhitespace(rng span.Range, tok *token.File, content []byte) s
 	rng.Start = tok.Pos(offset)
 
 	offset = tok.Offset(rng.End)
-	for offset-1 >= 0 {
-		if !unicode.IsSpace(rune(content[offset-1])) {
+	for o := offset - 1; 0 <= o && o < len(content); {
+		if !unicode.IsSpace(rune(content[o])) {
 			break
 		}
 		// Move backwards one byte to find a non-whitespace character.

commit 5f472bff4deea95cb9f592bbb0d427be97106f65

internal/lsp/source: fix bug introduced by CL 246757
CL 246757 resulted in an infinite loop because the value of "o" is
never updated.

Change-Id: I79cf265349838de19089c4468128c565a9a3cda3
Reviewed-on: https://go-review.googlesource.com/c/tools/+/247182
Run-TryBot: Rebecca Stambler <rstambler@golang.org>
TryBot-Result: Gobot Gobot <gobot@golang.org>
Reviewed-by: Josh Baum <joshbaum@google.com>

internal/lsp/source/extract.go
@@ -476,13 +476,13 @@ func adjustRangeForWhitespace(rng span.Range, tok *token.File, content []byte) s
 	}
 	rng.Start = tok.Pos(offset)
 
+	// Move backwards to find a non-whitespace character.
 	offset = tok.Offset(rng.End)
-	for o := offset - 1; 0 <= o && o < len(content); {
+	for o := offset - 1; 0 <= o && o < len(content); o-- {
 		if !unicode.IsSpace(rune(content[o])) {
 			break
 		}
-		// Move backwards one byte to find a non-whitespace character.
-		offset -= 1
+		offset = o
 	}
 	rng.End = tok.Pos(offset)
 	return rng