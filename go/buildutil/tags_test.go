559d77be8c186cce135adf7b3f731291674b9162
Mark Billie <mbillie@digitalocean.com>
2021-03-04 21:49:00 -0700

    add test for comma-delimited tags

--- go/buildutil/tags_test.go
+++ go/buildutil/tags_test.go
@@ -30,3 +30,21 @@ func TestTags(t *testing.T) {
 		t.Errorf("f.Args() = %q, want %q", f.Args(), want)
 	}
 }
+
+func TestTagsWithCommaDelimiter(t *testing.T) {
+	f := flag.NewFlagSet("TestTags", flag.PanicOnError)
+	var ctxt build.Context
+	f.Var((*buildutil.TagsFlag)(&ctxt.BuildTags), "tags", buildutil.TagsFlagDoc)
+	f.Parse([]string{"-tags", `'one,two,three,four'`, "rest"})
+
+	// BuildTags
+	want := []string{"one", "two", "three", "four"}
+	if !reflect.DeepEqual(ctxt.BuildTags, want) {
+		t.Errorf("BuildTags = %q, want %q", ctxt.BuildTags, want)
+	}
+
+	// Args()
+	if want := []string{"rest"}; !reflect.DeepEqual(f.Args(), want) {
+		t.Errorf("f.Args() = %q, want %q", f.Args(), want)
+	}
+}
