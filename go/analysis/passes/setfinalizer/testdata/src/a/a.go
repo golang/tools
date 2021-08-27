f5516474dcbcceda81c3fdf9048cc879a7188883
KimMachineGun <geon0250@gmail.com>
2021-08-28 02:44:28 +0900

    go/analysis/passes/setfinalizer: add test cases

--- go/analysis/passes/setfinalizer/testdata/src/a/a.go
+++ go/analysis/passes/setfinalizer/testdata/src/a/a.go
@@ -14,6 +14,10 @@ type Tint int
 type Tintptr *int
 type Tintptr2 *Tint
 type NonFunc struct{}
+type IntFinalizer func(*int)
+type TintFinalizer func(*Tint)
+type TintptrFinalizer func(Tintptr)
+type Tintptr2Finalizer func(Tintptr2)
 
 func _() {
 	// check first arg
@@ -35,6 +39,10 @@ func _() {
 	runtime.SetFinalizer(&i, func(*Tint) {}) // want "cannot pass \\*int to finalizer func\\(\\*a\\.Tint\\)"
 	runtime.SetFinalizer(&i, func(Tintptr) {})
 	runtime.SetFinalizer(&i, func(Tintptr2) {}) // want "cannot pass \\*int to finalizer func\\(a\\.Tintptr2\\)"
+	runtime.SetFinalizer(&i, IntFinalizer(nil))
+	runtime.SetFinalizer(&i, TintFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass \\*int to finalizer a\\.TintFinalizer"
+	runtime.SetFinalizer(&i, TintptrFinalizer(nil))
+	runtime.SetFinalizer(&i, Tintptr2Finalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass \\*int to finalizer a\\.Tintptr2Finalizer"
 	var ti Tint
 	runtime.SetFinalizer(&ti, NonFunc{})               // want "runtime\\.SetFinalizer: second argument is a\\.NonFunc, not a function"
 	runtime.SetFinalizer(&ti, func(...interface{}) {}) // want "runtime\\.SetFinalizer: cannot pass \\*a\\.Tint to finalizer func\\(\\.\\.\\.interface{}\\) because dotdotdot"
@@ -43,6 +51,10 @@ func _() {
 	runtime.SetFinalizer(&ti, func(*Tint) {})
 	runtime.SetFinalizer(&ti, func(Tintptr) {}) // want "cannot pass \\*a\\.Tint to finalizer func\\(a\\.Tintptr\\)"
 	runtime.SetFinalizer(&ti, func(Tintptr2) {})
+	runtime.SetFinalizer(&ti, IntFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass \\*a\\.Tint to finalizer a\\.IntFinalizer"
+	runtime.SetFinalizer(&ti, TintFinalizer(nil))
+	runtime.SetFinalizer(&ti, TintptrFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass \\*a\\.Tint to finalizer a\\.TintptrFinalizer"
+	runtime.SetFinalizer(&ti, Tintptr2Finalizer(nil))
 	tip := Tintptr(&i)
 	runtime.SetFinalizer(tip, NonFunc{})               // want "runtime\\.SetFinalizer: second argument is a\\.NonFunc, not a function"
 	runtime.SetFinalizer(tip, func(...interface{}) {}) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr to finalizer func\\(\\.\\.\\.interface{}\\) because dotdotdot"
@@ -51,6 +63,10 @@ func _() {
 	runtime.SetFinalizer(tip, func(*Tint) {}) // want "cannot pass a\\.Tintptr to finalizer func\\(\\*a\\.Tint\\)"
 	runtime.SetFinalizer(tip, func(Tintptr) {})
 	runtime.SetFinalizer(tip, func(Tintptr2) {}) // want "cannot pass a\\.Tintptr to finalizer func\\(a\\.Tintptr2\\)"
+	runtime.SetFinalizer(tip, IntFinalizer(nil))
+	runtime.SetFinalizer(tip, TintFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr to finalizer a\\.TintFinalizer"
+	runtime.SetFinalizer(tip, TintptrFinalizer(nil))
+	runtime.SetFinalizer(tip, Tintptr2Finalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr to finalizer a\\.Tintptr2Finalizer"
 	tip2 := Tintptr2(&ti)
 	runtime.SetFinalizer(tip2, NonFunc{})               // want "runtime\\.SetFinalizer: second argument is a\\.NonFunc, not a function"
 	runtime.SetFinalizer(tip2, func(...interface{}) {}) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr2 to finalizer func\\(\\.\\.\\.interface{}\\) because dotdotdot"
@@ -59,4 +75,8 @@ func _() {
 	runtime.SetFinalizer(tip2, func(*Tint) {})
 	runtime.SetFinalizer(tip2, func(Tintptr) {}) // want "cannot pass a\\.Tintptr2 to finalizer func\\(a\\.Tintptr\\)"
 	runtime.SetFinalizer(tip2, func(Tintptr2) {})
+	runtime.SetFinalizer(tip2, IntFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr2 to finalizer a\\.IntFinalizer"
+	runtime.SetFinalizer(tip2, TintFinalizer(nil))
+	runtime.SetFinalizer(tip2, TintptrFinalizer(nil)) // want "runtime\\.SetFinalizer: cannot pass a\\.Tintptr2 to finalizer a\\.TintptrFinalizer"
+	runtime.SetFinalizer(tip2, Tintptr2Finalizer(nil))
 }
