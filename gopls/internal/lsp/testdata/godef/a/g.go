a7fbd1e1edc82ace6776b5261c238ca322208a6a
Nikita Shoshin <shoshin_nikita@fastmail.com>
2023-03-31 00:14:35 +0400

    gopls/internal/lsp/source: show both the original declaration and the value of constants in hover
    This change improves the hover information for constants by showing both
    the original declaration and the value. The value is displayed as
    an inline comment. If the original declaration and the value are the same,
    there will be no inline comment.
    
    Examples:
    
    ```go
    const duration time.Duration = 15*time.Minute + 10*time.Second // 15m10s
    
    const octal untyped int = 0o777 // 511
    
    const expr untyped int = 2 << (0b111&0b101 - 2) // 16
    
    const boolean untyped bool = (55 - 3) == (26 * 2) // true
    
    const dec untyped int = 500
    ```
    
    Other changes:
    
    * Calls to `objectString` that format methods or variables have been
      replaced with `types.ObjectString`.
    * The logic of inferred signature formatting has been extracted from
      `objectString` to a new function `inferredSignatureString`.
    * Remove unused function `extractFieldList`.
    
    Fixes golang/go#47453

--- gopls/internal/lsp/testdata/godef/a/g.go
+++ gopls/internal/lsp/testdata/godef/a/g.go
@@ -1,6 +1,66 @@
 package a
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // dur is a constant of type time.Duration.
 const dur = 15*time.Minute + 10*time.Second + 350*time.Millisecond //@dur,hoverdef("dur", dur)
+
+// Numbers.
+func _() {
+	const hex, bin = 0xe34e, 0b1001001
+
+	const (
+		// no inline comment
+		decimal = 153
+
+		numberWithUnderscore int64 = 10_000_000_000
+		octal                      = 0o777
+		expr                       = 2 << (0b111&0b101 - 2)
+		boolean                    = (55 - 3) == (26 * 2)
+	)
+
+	_ = decimal              //@mark(decimalConst, "decimal"),hoverdef("decimal", decimalConst)
+	_ = hex                  //@mark(hexConst, "hex"),hoverdef("hex", hexConst)
+	_ = bin                  //@mark(binConst, "bin"),hoverdef("bin", binConst)
+	_ = numberWithUnderscore //@mark(numberWithUnderscoreConst, "numberWithUnderscore"),hoverdef("numberWithUnderscore", numberWithUnderscoreConst)
+	_ = octal                //@mark(octalConst, "octal"),hoverdef("octal", octalConst)
+	_ = expr                 //@mark(exprConst, "expr"),hoverdef("expr", exprConst)
+	_ = boolean              //@mark(boolConst, "boolean"),hoverdef("boolean", boolConst)
+
+	const ln10 = 2.30258509299404568401799145468436420760110148862877297603332790
+
+	_ = ln10 //@mark(ln10Const, "ln10"),hoverdef("ln10", ln10Const)
+}
+
+// Iota.
+func _() {
+	const (
+		a = 1 << iota
+		b
+	)
+
+	_ = a //@mark(aIota, "a"),hoverdef("a", aIota)
+	_ = b //@mark(bIota, "b"),hoverdef("b", bIota)
+}
+
+// Strings.
+func _() {
+	const (
+		str     = "hello" + " " + "world"
+		longStr = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget ipsum non nunc
+molestie mattis id quis augue. Mauris dictum tincidunt ipsum, in auctor arcu congue eu.
+Morbi hendrerit fringilla libero commodo varius. Vestibulum in enim rutrum, rutrum tellus
+aliquet, luctus enim. Nunc sem ex, consectetur id porta nec, placerat vel urna.`
+	)
+
+	_ = str     //@mark(strConst, "str"),hoverdef("str", strConst)
+	_ = longStr //@mark(longStrConst, "longStr"),hoverdef("longStr", longStrConst)
+}
+
+// Constants from other packages.
+func _() {
+	_ = math.MaxFloat32 //@mark(maxFloat32Const, "MaxFloat32"),hoverdef("MaxFloat32", maxFloat32Const)
+}
