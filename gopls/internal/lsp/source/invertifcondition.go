d2ec2fc77352bbf0c70922d8384ea0f8f393f0ad
Johan Walles <johan.walles@gmail.com>
2023-04-01 16:39:12 +0200

    Replace the else block with the if block

--- gopls/internal/lsp/source/invertifcondition.go
+++ gopls/internal/lsp/source/invertifcondition.go
@@ -11,16 +11,34 @@ import (
 )
 
 func invertIfCondition(fset *token.FileSet, start, end token.Pos, src []byte, file *ast.File, pkg *types.Package, info *types.Info) (*analysis.SuggestedFix, error) {
+	ifStatement, _, _, err := CanInvertIfCondition(start, end, file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the text of the first (if) block
+	bodyPosInSource := fset.PositionFor(ifStatement.Body.Lbrace, false)
+	bodyEndInSource := fset.PositionFor(ifStatement.Body.Rbrace, false)
+	bodyText := src[bodyPosInSource.Offset : bodyEndInSource.Offset+1]
+
+	// Create a TextEdit for replacing the second block with the contents of the first block
+	replaceElseWithBody := analysis.TextEdit{
+		Pos:     ifStatement.Else.Pos(),
+		End:     ifStatement.Else.End(),
+		NewText: bodyText,
+	}
+
+	// Return a SuggestedFix with just that TextEdit in there
 	return &analysis.SuggestedFix{
 		Message: "Invert if condition", // FIXME: Try without this message and see how it looks!
 		TextEdits: []analysis.TextEdit{
-			{
-				Pos:     start,
-				End:     end,
-				NewText: []byte("FIXME: Imagine an inverted if statement here"),
-			},
+			replaceElseWithBody,
 		},
 	}, nil
+
+	// FIXME: Also make a TextEdit for replacing the first block with the second block
+
+	// FIXME: Also make a TextEdit for inverting the if condition
 }
 
 // CanInvertIfCondition reports whether we can do invert-if-condition on the
