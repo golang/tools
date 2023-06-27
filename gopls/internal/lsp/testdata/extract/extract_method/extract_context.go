d4a72239efb5113291443ef867a80bd357de9a93
Viktor Stanchev <viktor@anchorlabs.com>
2023-06-27 18:36:25 +0000

    add failing tests for parameter sorting

--- gopls/internal/lsp/testdata/extract/extract_method/extract_context.go
+++ gopls/internal/lsp/testdata/extract/extract_method/extract_context.go
@@ -2,12 +2,19 @@ package extract
 
 import "context"
 
-type A struct {
+type B struct {
 	x int
 	y int
 }
 
-func (a *A) AddP(ctx context.Context) (int, error) {
-	sum := a.x + a.y
+func (b *B) AddP(ctx context.Context) (int, error) {
+	sum := b.x + b.y
 	return sum, ctx.Err() //@extractmethod("return", "ctx.Err()"),extractfunc("return", "ctx.Err()")
+}
+
+func (b *B) LongList(ctx context.Context) (int, error) {
+	p1 := 1
+	p2 := 1
+	p3 := 1
+	return p1 + p2 + p3, ctx.Err() //@extractmethod("return", "ctx.Err()"),extractfunc("return", "ctx.Err()")
 }
