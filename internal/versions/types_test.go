3b6876f0e6b6b041bdffd29db4b100b0794bc62b
Tim King <taking@google.com>
2023-10-05 14:41:16 -0700

    internal/versions: add a new versions package
    Adds a new versions package to provide x/tools a way to
    deal with new GoVersion() and FileVersions API from go/types
    and the new go/version standard library.
    
    This provides a stable API until 1.26.
    
    Updates golang/go#63374
    Updates golang/go#62605
    
    Change-Id: I4de54df00ea0f4363c0383cbdc917186277bfd0a
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/533056
    Run-TryBot: Tim King <taking@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

---
+++ internal/versions/types_test.go
@@ -0,0 +1,87 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package versions_test
+
+import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"testing"
+
+	"golang.org/x/tools/internal/testenv"
+	"golang.org/x/tools/internal/versions"
+)
+
+func Test(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+
+	var contents = map[string]string{
+		"gobuild.go": `
+	//go:build go1.23
+	package p
+	`,
+		"noversion.go": `
+	package p
+	`,
+	}
+	type fileTest struct {
+		fname string
+		want  string
+	}
+	for _, item := range []struct {
+		goversion string
+		pversion  string
+		tests     []fileTest
+	}{
+		{"", "", []fileTest{{"noversion.go", ""}, {"gobuild.go", ""}}},
+		{"go1.22", "go1.22", []fileTest{{"noversion.go", "go1.22"}, {"gobuild.go", "go1.23"}}},
+	} {
+		name := fmt.Sprintf("types.Config{GoVersion:%q}", item.goversion)
+		t.Run(name, func(t *testing.T) {
+			fset := token.NewFileSet()
+			files := make([]*ast.File, len(item.tests))
+			for i, test := range item.tests {
+				files[i] = parse(t, fset, test.fname, contents[test.fname])
+			}
+			pkg, info := typeCheck(t, fset, files, item.goversion)
+			if got, want := versions.GoVersion(pkg), item.pversion; versions.Compare(got, want) != 0 {
+				t.Errorf("GoVersion()=%q. expected %q", got, want)
+			}
+			if got := versions.FileVersions(info, nil); got != "" {
+				t.Errorf(`FileVersions(nil)=%q. expected ""`, got)
+			}
+			for i, test := range item.tests {
+				if got, want := versions.FileVersions(info, files[i]), test.want; got != want {
+					t.Errorf("FileVersions(%s)=%q. expected %q", test.fname, got, want)
+				}
+			}
+		})
+	}
+}
+
+func parse(t *testing.T, fset *token.FileSet, name, src string) *ast.File {
+	file, err := parser.ParseFile(fset, name, src, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return file
+}
+
+func typeCheck(t *testing.T, fset *token.FileSet, files []*ast.File, goversion string) (*types.Package, *types.Info) {
+	conf := types.Config{
+		Importer:  importer.Default(),
+		GoVersion: goversion,
+	}
+	info := types.Info{}
+	versions.InitFileVersions(&info)
+	pkg, err := conf.Check("", fset, files, &info)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return pkg, &info
+}
---
+++ internal/versions/versions_test.go
@@ -0,0 +1,137 @@
+// Copyright 2023 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package versions_test
+
+import (
+	"testing"
+
+	"golang.org/x/tools/internal/testenv"
+	"golang.org/x/tools/internal/versions"
+)
+
+func TestIsValid(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+
+	// valid versions
+	for _, x := range []string{
+		"go1.21",
+		"go1.21.2",
+		"go1.21rc",
+		"go1.21rc2",
+		"go0.0", // ??
+		"go1",
+		"go2",
+	} {
+		if !versions.IsValid(x) {
+			t.Errorf("expected versions.IsValid(%q) to hold", x)
+		}
+	}
+
+	// invalid versions
+	for _, x := range []string{
+		"",
+		"bad",
+		"1.21",
+		"v1.21",
+		"go",
+		"goAA",
+		"go2_3",
+		"go1.BB",
+		"go1.21.",
+		"go1.21.2_2",
+		"go1.21rc_2",
+		"go1.21rc2_",
+	} {
+		if versions.IsValid(x) {
+			t.Errorf("expected versions.IsValid(%q) to not hold", x)
+		}
+	}
+}
+
+func TestVersionComparisons(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+
+	for _, item := range []struct {
+		x, y string
+		want int
+	}{
+		{"go2", "go2", 0},
+		{"go2", "go1.21.2", +1},
+		{"go2", "go1.21rc2", +1},
+		{"go2", "go1.21rc", +1},
+		{"go2", "go1.21", +1},
+		{"go2", "go1", +1},
+		{"go2", "go0.0", +1},
+		{"go2", "", +1},
+		{"go2", "bad", +1},
+		{"go1.21.2", "go1.21.2", 0},
+		{"go1.21.2", "go1.21rc2", +1},
+		{"go1.21.2", "go1.21rc", +1},
+		{"go1.21.2", "go1.21", +1},
+		{"go1.21.2", "go1", +1},
+		{"go1.21.2", "go0.0", +1},
+		{"go1.21.2", "", +1},
+		{"go1.21.2", "bad", +1},
+		{"go1.21rc2", "go1.21rc2", 0},
+		{"go1.21rc2", "go1.21rc", +1},
+		{"go1.21rc2", "go1.21", +1},
+		{"go1.21rc2", "go1", +1},
+		{"go1.21rc2", "go0.0", +1},
+		{"go1.21rc2", "", +1},
+		{"go1.21rc2", "bad", +1},
+		{"go1.21rc", "go1.21rc", 0},
+		{"go1.21rc", "go1.21", +1},
+		{"go1.21rc", "go1", +1},
+		{"go1.21rc", "go0.0", +1},
+		{"go1.21rc", "", +1},
+		{"go1.21rc", "bad", +1},
+		{"go1.21", "go1.21", 0},
+		{"go1.21", "go1", +1},
+		{"go1.21", "go0.0", +1},
+		{"go1.21", "", +1},
+		{"go1.21", "bad", +1},
+		{"go1", "go1", 0},
+		{"go1", "go0.0", +1},
+		{"go1", "", +1},
+		{"go1", "bad", +1},
+		{"go0.0", "go0.0", 0},
+		{"go0.0", "", +1},
+		{"go0.0", "bad", +1},
+		{"", "", 0},
+		{"", "bad", 0},
+		{"bad", "bad", 0},
+	} {
+		got := versions.Compare(item.x, item.y)
+		if got != item.want {
+			t.Errorf("versions.Compare(%q, %q)=%d. expected %d", item.x, item.y, got, item.want)
+		}
+		reverse := versions.Compare(item.y, item.x)
+		if reverse != -got {
+			t.Errorf("versions.Compare(%q, %q)=%d. expected %d", item.y, item.x, reverse, -got)
+		}
+	}
+}
+
+func TestLang(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+	for _, item := range []struct {
+		x    string
+		want string
+	}{
+		// valid
+		{"go1.21rc2", "go1.21"},
+		{"go1.21.2", "go1.21"},
+		{"go1.21", "go1.21"},
+		{"go1", "go1"},
+		// invalid
+		{"bad", ""},
+		{"1.21", ""},
+	} {
+		if got := versions.Lang(item.x); got != item.want {
+			t.Errorf("versions.Lang(%q)=%q. expected %q", item.x, got, item.want)
+		}
+	}
+
+}
