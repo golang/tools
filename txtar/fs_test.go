3e6aa587f2dde101868728cc0e1ab03e5c11c3a5
Carl Johnson <me@carlmjohnson.net>
2023-10-24 15:37:55 -0400

    Test

--- txtar/fs_test.go
+++ txtar/fs_test.go
@@ -15,34 +15,33 @@ import (
 	"testing/iotest"
 )
 
-func TestFS(t *testing.T) {
-	for _, tc := range []struct{ name, input, files string }{
-		{
-			name:  "empty",
-			input: ``,
-			files: "",
-		},
-		{
-			name: "one",
-			input: `
+var fstestcases = []struct{ name, input, files string }{
+	{
+		name:  "empty",
+		input: ``,
+		files: "",
+	},
+	{
+		name: "one",
+		input: `
 -- one.txt --
 one
 `,
-			files: "one.txt",
-		},
-		{
-			name: "two",
-			input: `
+		files: "one.txt",
+	},
+	{
+		name: "two",
+		input: `
 -- one.txt --
 one
 -- two.txt --
 two
 `,
-			files: "one.txt two.txt",
-		},
-		{
-			name: "subdirectories",
-			input: `
+		files: "one.txt two.txt",
+	},
+	{
+		name: "subdirectories",
+		input: `
 -- one.txt --
 one
 -- 2/two.txt --
@@ -52,80 +51,94 @@ three
 -- 4/four.txt --
 three
 `,
-			files: "one.txt 2/two.txt 2/3/three.txt 4/four.txt",
-		},
-		{
-			name: "unclean file names",
-			input: `
+		files: "one.txt 2/two.txt 2/3/three.txt 4/four.txt",
+	},
+	{
+		name: "unclean file names",
+		input: `
 -- 1/../one.txt --
 one
 -- 2/sub/../two.txt --
 two
 `,
-			files: "one.txt 2/two.txt",
-		},
-	} {
+		files: "one.txt 2/two.txt",
+	},
+}
+
+func TestFS(t *testing.T) {
+	for _, tc := range fstestcases {
 		t.Run(tc.name, func(t *testing.T) {
-			a := Parse([]byte(tc.input))
-			files := strings.Fields(tc.files)
-			if err := fstest.TestFS(a.FS(), files...); err != nil {
+			testFS(t, tc.name, tc.input, tc.files)
+		})
+	}
+}
+
+func FuzzFS(f *testing.F) {
+	for _, tc := range fstestcases {
+		f.Add(tc.name, tc.input, tc.files)
+	}
+	f.Fuzz(testFS)
+}
+
+func testFS(t *testing.T, name, input, fileNames string) {
+	a := Parse([]byte(input))
+	files := strings.Fields(fileNames)
+	if err := fstest.TestFS(a.FS(), files...); err != nil {
+		t.Fatal(err)
+	}
+	for _, name := range files {
+		for _, f := range a.Files {
+			if f.Name != name {
+				continue
+			}
+			fsys := a.FS()
+			b, err := fs.ReadFile(fsys, name)
+			if err != nil {
 				t.Fatal(err)
 			}
-			for _, name := range files {
-				for _, f := range a.Files {
-					if f.Name != name {
-						continue
-					}
-					fsys := a.FS()
-					b, err := fs.ReadFile(fsys, name)
-					if err != nil {
-						t.Fatal(err)
-					}
-					if string(b) != string(f.Data) {
-						t.Fatalf("mismatched contents for %q", name)
-					}
-					// Be careful with n cases, this open is O(n^3) deep
-					// Do iotest
-					fsfile, err := fsys.Open(name)
-					if err != nil {
-						t.Fatal(err)
-					}
-					if err = iotest.TestReader(fsfile, f.Data); err != nil {
-						t.Fatal(err)
-					}
-					if err = fsfile.Close(); err != nil {
-						t.Fatal(err)
-					}
-					// test io.Copy
-					fsfile, err = fsys.Open(name)
-					if err != nil {
-						t.Fatal(err)
-					}
-					var buf strings.Builder
-					n, err := io.Copy(&buf, fsfile)
-					if err != nil {
-						t.Fatal(err)
-					}
-					if n != int64(len(f.Data)) {
-						t.Fatalf("bad copy size: %d", n)
-					}
-					if buf.String() != string(f.Data) {
-						t.Fatalf("mismatched contents for io.Copy of %q", name)
-					}
-					if err = fsfile.Close(); err != nil {
-						t.Fatal(err)
-					}
-				}
+			if string(b) != string(f.Data) {
+				t.Fatalf("mismatched contents for %q", name)
 			}
-			a2, err := From(a.FS())
+			// Be careful with n cases, this open is O(n^3) deep
+			// Do iotest
+			fsfile, err := fsys.Open(name)
 			if err != nil {
-				t.Fatalf("failed to write fsys for %v: %v", tc.name, err)
+				t.Fatal(err)
 			}
+			if err = iotest.TestReader(fsfile, f.Data); err != nil {
+				t.Fatal(err)
+			}
+			if err = fsfile.Close(); err != nil {
+				t.Fatal(err)
+			}
+			// test io.Copy
+			fsfile, err = fsys.Open(name)
+			if err != nil {
+				t.Fatal(err)
+			}
+			var buf strings.Builder
+			n, err := io.Copy(&buf, fsfile)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if n != int64(len(f.Data)) {
+				t.Fatalf("bad copy size: %d", n)
+			}
+			if buf.String() != string(f.Data) {
+				t.Fatalf("mismatched contents for io.Copy of %q", name)
+			}
+			if err = fsfile.Close(); err != nil {
+				t.Fatal(err)
+			}
+		}
+	}
+	a2, err := From(a.FS())
+	if err != nil {
+		t.Fatalf("failed to write fsys for %v: %v", name, err)
+	}
 
-			if in, out := normalized(a), normalized(a2); in != out {
-				t.Errorf("From round trip failed: %q != %q", in, out)
-			}
-		})
+	if in, out := normalized(a), normalized(a2); in != out {
+		t.Errorf("From round trip failed: %q != %q", in, out)
 	}
 }
 
