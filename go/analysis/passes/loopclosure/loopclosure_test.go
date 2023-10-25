cd62b43375f8422d493d33821c68f218be41a012
Tim King <taking@google.com>
2023-10-25 09:34:17 -0700

    go/analysis/passes/loopclosure: disable checker after go1.22.
    Uses the (*types.Info).FileVersion to disable the loopclosure checker
    when in an *ast.File that uses GoVersion >= 1.22.
    
    Updates golang/go#62605
    Updates golang/go#63888
    
    Change-Id: I2ebe974bc2ee2323eafb0f02d455ab76b3b9268d
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/539016
    Run-TryBot: Tim King <taking@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    TryBot-Result: Gopher Robot <gobot@golang.org>

--- go/analysis/passes/loopclosure/loopclosure_test.go
+++ go/analysis/passes/loopclosure/loopclosure_test.go
@@ -5,11 +5,16 @@
 package loopclosure_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
+	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/analysistest"
 	"golang.org/x/tools/go/analysis/passes/loopclosure"
+	"golang.org/x/tools/internal/testenv"
 	"golang.org/x/tools/internal/typeparams"
+	"golang.org/x/tools/txtar"
 )
 
 func Test(t *testing.T) {
@@ -19,4 +24,44 @@ func Test(t *testing.T) {
 		tests = append(tests, "typeparams")
 	}
 	analysistest.Run(t, testdata, loopclosure.Analyzer, tests...)
+}
+
+func TestVersions22(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+
+	testfile := filepath.Join(analysistest.TestData(), "src", "versions", "go22.txtar")
+	runTxtarFile(t, testfile, loopclosure.Analyzer, "golang.org/fake/versions")
+}
+
+func TestVersions18(t *testing.T) {
+	testenv.NeedsGo1Point(t, 18)
+
+	testfile := filepath.Join(analysistest.TestData(), "src", "versions", "go18.txtar")
+	runTxtarFile(t, testfile, loopclosure.Analyzer, "golang.org/fake/versions")
+}
+
+// runTxtarFile unpacks a txtar archive to a directory, and runs
+// analyzer on the given patterns.
+//
+// This is compatible with a go.mod file.
+//
+// TODO(taking): Consider unifying with analysistest.
+func runTxtarFile(t *testing.T, path string, analyzer *analysis.Analyzer, patterns ...string) {
+	ar, err := txtar.ParseFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dir := t.TempDir()
+	for _, file := range ar.Files {
+		name, content := file.Name, file.Data
+
+		filename := filepath.Join(dir, name)
+		os.MkdirAll(filepath.Dir(filename), 0777) // ignore error
+		if err := os.WriteFile(filename, content, 0666); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	analysistest.Run(t, dir, analyzer, patterns...)
 }
--- gopls/doc/analyzers.md
+++ gopls/doc/analyzers.md
@@ -276,8 +276,12 @@ iteration variable of an enclosing loop, and the loop calls the function
 in such a way (e.g. with go or defer) that it may outlive the loop
 iteration and possibly observe the wrong value of the variable.
 
+Note: An iteration variable can only outlive a loop iteration in Go versions <=1.21.
+In Go 1.22 and later, the loop variable lifetimes changed to create a new
+iteration variable per loop iteration. (See go.dev/issue/60078.)
+
 In this example, all the deferred functions run after the loop has
-completed, so all observe the final value of v.
+completed, so all observe the final value of v [<go1.22].
 
 	for _, v := range list {
 	    defer func() {
@@ -294,7 +298,10 @@ One fix is to create a new variable for each iteration of the loop:
 	    }()
 	}
 
-The next example uses a go statement and has a similar problem.
+After Go version 1.22, the previous two for loops are equivalent
+and both are correct.
+
+The next example uses a go statement and has a similar problem [<go1.22].
 In addition, it has a data race because the loop updates v
 concurrent with the goroutines accessing it.
 
@@ -318,7 +325,7 @@ A hard-to-spot variant of this form is common in parallel tests:
 	}
 
 The t.Parallel() call causes the rest of the function to execute
-concurrent with the loop.
+concurrent with the loop [<go1.22].
 
 The analyzer reports references only in the last statement,
 as it is not deep enough to understand the effects of subsequent
