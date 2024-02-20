a6289fe0c3fb86e172d15e1a1ea262b3a041c980
Robert Findley <rfindley@google.com>
2024-02-20 12:31:38 -0500

    [gopls-release-branch.0.15] gopls/internal/cache: fix two bugs related to workspace packages
    Fix two bugs related to workspace packages that contributed to
    golang/go#65801.
    
    The first is that we didn't consider packages with open files to be
    workspace packages. This was pre-existing behavior, but in the past we
    simply relied on diagnoseChangedFiles to provide diagnostics for open
    packages, even though we didn't consider them to be part of the
    workspace. That led to races and inconsistent behavior: a change in one
    file would wipe out diagnostics in another, and so on. It's much simpler
    to say that if the package is open, it is part of the workspace. This
    leads to consistent behavior, no matter where diagnostics originate.
    
    The second bug is related to loading std and cmd. The new workspace
    package heuristics relied on go/packages.Package.Module to determine if
    a package is included in the workspace. For std and cmd, this field is
    nil (golang/go#65816), apparently due to limitations of `go list`. As a
    result, we were finding no workspace packages for std or cmd. Fix this
    by falling back to searching for the relevant go.mod file in the
    filesystem. Unfortunately this required reinstating the lockedSnapshot
    file source.
    
    These bugs revealed a rather large gap in our test coverage for std. Add
    a test that verifies that we compute std workspace packages.
    
    Fixes golang/go#65801
    
    Change-Id: Ic454d4a43e34af10e1224755a09d6c94c728c97d
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/565475
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    (cherry picked from commit 607b6641fe2fd4f4d22f30706e31719456a7e269)
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/565457

---
+++ gopls/internal/test/integration/workspace/std_test.go
@@ -0,0 +1,73 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package workspace
+
+import (
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	. "golang.org/x/tools/gopls/internal/test/integration"
+)
+
+func TestStdWorkspace(t *testing.T) {
+	// This test checks that we actually load workspace packages when opening
+	// GOROOT.
+	//
+	// In golang/go#65801, we failed to do this because go/packages returns nil
+	// Module for std and cmd.
+	//
+	// Because this test loads std as a workspace, it may be slow on smaller
+	// builders.
+	if testing.Short() {
+		t.Skip("skipping with -short: loads GOROOT")
+	}
+
+	// The test also fails on Windows because an absolute path does not match
+	// (likely a misspelling due to slashes).
+	// TODO(rfindley): investigate and fix this on windows.
+	if runtime.GOOS == "windows" {
+		t.Skip("skipping on windows: fails to to misspelled paths")
+	}
+
+	// Query GOROOT. This is slightly more precise than e.g. runtime.GOROOT, as
+	// it queries the go command in the environment.
+	goroot, err := exec.Command("go", "env", "GOROOT").Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+	stdDir := filepath.Join(strings.TrimSpace(string(goroot)), "src")
+	WithOptions(
+		Modes(Default), // This test may be slow. No reason to run it multiple times.
+		WorkspaceFolders(stdDir),
+	).Run(t, "", func(t *testing.T, env *Env) {
+		// Find parser.ParseFile. Query with `'` to get an exact match.
+		syms := env.Symbol("'go/parser.ParseFile")
+		if len(syms) != 1 {
+			t.Fatalf("got %d symbols, want exactly 1. Symbols:\n%v", len(syms), syms)
+		}
+		parserPath := syms[0].Location.URI.Path()
+		env.OpenFile(parserPath)
+
+		// Find the reference to ast.File from the signature of ParseFile. This
+		// helps guard against matching a comment.
+		astFile := env.RegexpSearch(parserPath, `func ParseFile\(.*ast\.(File)`)
+		refs := env.References(astFile)
+
+		// If we've successfully loaded workspace packages for std, we should find
+		// a reference in go/types.
+		foundGoTypesReference := false
+		for _, ref := range refs {
+			if strings.Contains(string(ref.URI), "go/types") {
+				foundGoTypesReference = true
+			}
+		}
+		if !foundGoTypesReference {
+			t.Errorf("references(ast.File) did not return a go/types reference. Refs:\n%v", refs)
+		}
+	})
+}
