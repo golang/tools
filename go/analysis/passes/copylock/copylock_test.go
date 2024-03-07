dfcdf502203c0fcc5f4e2295b76cd7cf99477298
Tim King <taking@google.com>
2024-03-07 15:40:52 -0800

    go/analysis/passes/copylock: add support for ForStmt
    Adds diagnostics when ForStmt variables are copied by
    iteration in files with GoVersion>=1.22.
    
    Fixes golang/go#66387
    
    Change-Id: Id4aaaa7feb78bf89e5bf1320be3859af5998d8d0
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/569955
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- go/analysis/passes/copylock/copylock_test.go
+++ go/analysis/passes/copylock/copylock_test.go
@@ -5,13 +5,28 @@
 package copylock_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/internal/testenv"
+	"golang.org/x/tools/internal/testfiles"
 )
 
 func Test(t *testing.T) {
 	testdata := analysistest.TestData()
 	analysistest.Run(t, testdata, copylock.Analyzer, "a", "typeparams", "issue67787")
+}
+
+func TestVersions22(t *testing.T) {
+	testenv.NeedsGo1Point(t, 22)
+
+	dir := testfiles.ExtractTxtarFileToTmp(t, filepath.Join(analysistest.TestData(), "src", "forstmt", "go22.txtar"))
+	analysistest.Run(t, dir, copylock.Analyzer, "golang.org/fake/forstmt")
+}
+
+func TestVersions21(t *testing.T) {
+	dir := testfiles.ExtractTxtarFileToTmp(t, filepath.Join(analysistest.TestData(), "src", "forstmt", "go21.txtar"))
+	analysistest.Run(t, dir, copylock.Analyzer, "golang.org/fake/forstmt")
 }
