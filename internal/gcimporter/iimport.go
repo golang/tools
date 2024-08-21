9ef0547234875fc17df56c6329b5c647753cbfe5
Tim King <taking@google.com>
2024-08-21 10:13:22 -0700

    internal/gcimporter: move indexed format docs
    Move the documentation for indexed export data format from
      $GOROOT/src/cmd/compile/internal/typecheck/iexport.go
    to
      x/tools/internal/gcimporter/iexport.go .
    This is the only current writer of this format.
    
    Updates golang/go#68778
    Updates golang/go#68898
    
    Change-Id: I365f56315d03affc5860cf407ea6e3d0caa1a88e
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/607495
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

--- internal/gcimporter/iimport.go
+++ internal/gcimporter/iimport.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // Indexed package import.
-// See cmd/compile/internal/gc/iexport.go for the export data format.
+// See iexport.go for the export data format.
 
 // This file is a copy of $GOROOT/src/go/internal/gcimporter/iimport.go.
 
