1c3716656e6ef45cdb3ff8c018856a44750165c4
Tim King <taking@google.com>
2024-09-19 14:11:53 -0700

    go/ssa: move build tag guarded tests
    Move tests out of files that have build tag guards that
    are now true for all supported versions.
    
    Change-Id: Ia8c5c8d20aa11991288a65eab72146adac60a82a
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/614457
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- go/ssa/builder_test.go
+++ go/ssa/builder_test.go
@@ -25,6 +25,7 @@ import (
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/tools/go/analysis/analysistest"
 	"golang.org/x/tools/go/buildutil"
+	"golang.org/x/tools/go/expect"
 	"golang.org/x/tools/go/loader"
 	"golang.org/x/tools/go/packages"
 	"golang.org/x/tools/go/ssa"
@@ -1405,4 +1406,263 @@ func constString(v ssa.Value) string {
 		return strings.Trim(str, `"`)
 	}
 	return "<not a constant string>"
+}
+
+// TestMultipleGoversions tests that globals initialized to equivalent
+// function literals are compiled based on the different GoVersion in each file.
+func TestMultipleGoversions(t *testing.T) {
+	var contents = map[string]string{
+		"post.go": `
+	//go:build go1.22
+	package p
+
+	var distinct = func(l []int) {
+		for i := range l {
+			print(&i)
+		}
+	}
+	`,
+		"pre.go": `
+	package p
+
+	var same = func(l []int) {
+		for i := range l {
+			print(&i)
+		}
+	}
+	`,
+	}
+
+	fset := token.NewFileSet()
+	var files []*ast.File
+	for _, fname := range []string{"post.go", "pre.go"} {
+		file, err := parser.ParseFile(fset, fname, contents[fname], 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		files = append(files, file)
+	}
+
+	pkg := types.NewPackage("p", "")
+	conf := &types.Config{Importer: nil, GoVersion: "go1.21"}
+	p, _, err := ssautil.BuildPackage(conf, fset, pkg, files, ssa.SanityCheckFunctions)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Test that global is initialized to a function literal that was
+	// compiled to have the expected for loop range variable lifetime for i.
+	for _, test := range []struct {
+		global *ssa.Global
+		want   string // basic block to []*ssa.Alloc.
+	}{
+		{p.Var("same"), "map[entry:[new int (i)]]"},               // i is allocated in the entry block.
+		{p.Var("distinct"), "map[rangeindex.body:[new int (i)]]"}, // i is allocated in the body block.
+	} {
+		// Find the function the test.name global is initialized to.
+		var fn *ssa.Function
+		for _, b := range p.Func("init").Blocks {
+			for _, instr := range b.Instrs {
+				if s, ok := instr.(*ssa.Store); ok && s.Addr == test.global {
+					fn, _ = s.Val.(*ssa.Function)
+				}
+			}
+		}
+		if fn == nil {
+			t.Fatalf("Failed to find *ssa.Function for initial value of global %s", test.global)
+		}
+
+		allocs := make(map[string][]string) // block comments -> []Alloc
+		for _, b := range fn.Blocks {
+			for _, instr := range b.Instrs {
+				if a, ok := instr.(*ssa.Alloc); ok {
+					allocs[b.Comment] = append(allocs[b.Comment], a.String())
+				}
+			}
+		}
+		if got := fmt.Sprint(allocs); got != test.want {
+			t.Errorf("[%s:=%s] expected the allocations to be in the basic blocks %q, got %q", test.global, fn, test.want, got)
+		}
+	}
+}
+
+// TestRangeOverInt tests that, in a range-over-int (#61405),
+// the type of each range var v (identified by print(v) calls)
+// has the expected type.
+func TestRangeOverInt(t *testing.T) {
+	testenv.NeedsGoExperiment(t, "range")
+
+	const rangeOverIntSrc = `
+		package p
+
+		type I uint8
+
+		func noKey(x int) {
+			for range x {
+				// does not crash
+			}
+		}
+
+		func untypedConstantOperand() {
+			for i := range 10 {
+				print(i) /*@ types("int")*/
+			}
+		}
+
+		func unsignedOperand(x uint64) {
+			for i := range x {
+				print(i) /*@ types("uint64")*/
+			}
+		}
+
+		func namedOperand(x I) {
+			for i := range x {
+				print(i)  /*@ types("p.I")*/
+			}
+		}
+
+		func typeparamOperand[T int](x T) {
+			for i := range x {
+				print(i)  /*@ types("T")*/
+			}
+		}
+
+		func assignment(x I) {
+			var k I
+			for k = range x {
+				print(k) /*@ types("p.I")*/
+			}
+		}
+	`
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "p.go", rangeOverIntSrc, parser.ParseComments)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pkg := types.NewPackage("p", "")
+	conf := &types.Config{}
+	p, _, err := ssautil.BuildPackage(conf, fset, pkg, []*ast.File{f}, ssa.SanityCheckFunctions)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Collect all notes in f, i.e. comments starting with "//@ types".
+	notes, err := expect.ExtractGo(fset, f)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Collect calls to the built-in print function.
+	fns := make(map[*ssa.Function]bool)
+	for _, mem := range p.Members {
+		if fn, ok := mem.(*ssa.Function); ok {
+			fns[fn] = true
+		}
+	}
+	probes := callsTo(fns, "print")
+	expectations := matchNotes(fset, notes, probes)
+
+	for call := range probes {
+		if expectations[call] == nil {
+			t.Errorf("Unmatched call: %v @ %s", call, fset.Position(call.Pos()))
+		}
+	}
+
+	// Check each expectation.
+	for call, note := range expectations {
+		var args []string
+		for _, a := range call.Args {
+			args = append(args, a.Type().String())
+		}
+		if got, want := fmt.Sprint(args), fmt.Sprint(note.Args); got != want {
+			at := fset.Position(call.Pos())
+			t.Errorf("%s: arguments to print had types %s, want %s", at, got, want)
+			logFunction(t, probes[call])
+		}
+	}
+}
+
+func TestBuildPackageGo120(t *testing.T) {
+	tests := []struct {
+		name     string
+		src      string
+		importer types.Importer
+	}{
+		{"slice to array", "package p; var s []byte; var _ = ([4]byte)(s)", nil},
+		{"slice to zero length array", "package p; var s []byte; var _ = ([0]byte)(s)", nil},
+		{"slice to zero length array type parameter", "package p; var s []byte; func f[T ~[0]byte]() { tmp := (T)(s); var z T; _ = tmp == z}", nil},
+		{"slice to non-zero length array type parameter", "package p; var s []byte; func h[T ~[1]byte | [4]byte]() { tmp := T(s); var z T; _ = tmp == z}", nil},
+		{"slice to maybe-zero length array type parameter", "package p; var s []byte; func g[T ~[0]byte | [4]byte]() { tmp := T(s); var z T; _ = tmp == z}", nil},
+		{
+			"rune sequence to sequence cast patterns", `
+			package p
+			// Each of fXX functions describes a 1.20 legal cast between sequences of runes
+			// as []rune, pointers to rune arrays, rune arrays, or strings.
+			//
+			// Comments listed given the current emitted instructions [approximately].
+			// If multiple conversions are needed, these are separated by |.
+			// rune was selected as it leads to string casts (byte is similar).
+			// The length 2 is not significant.
+			// Multiple array lengths may occur in a cast in practice (including 0).
+			func f00[S string, D string](s S)                               { _ = D(s) } // ChangeType
+			func f01[S string, D []rune](s S)                               { _ = D(s) } // Convert
+			func f02[S string, D []rune | string](s S)                      { _ = D(s) } // ChangeType | Convert
+			func f03[S [2]rune, D [2]rune](s S)                             { _ = D(s) } // ChangeType
+			func f04[S *[2]rune, D *[2]rune](s S)                           { _ = D(s) } // ChangeType
+			func f05[S []rune, D string](s S)                               { _ = D(s) } // Convert
+			func f06[S []rune, D [2]rune](s S)                              { _ = D(s) } // SliceToArrayPointer; Deref
+			func f07[S []rune, D [2]rune | string](s S)                     { _ = D(s) } // SliceToArrayPointer; Deref | Convert
+			func f08[S []rune, D *[2]rune](s S)                             { _ = D(s) } // SliceToArrayPointer
+			func f09[S []rune, D *[2]rune | string](s S)                    { _ = D(s) } // SliceToArrayPointer; Deref | Convert
+			func f10[S []rune, D *[2]rune | [2]rune](s S)                   { _ = D(s) } // SliceToArrayPointer | SliceToArrayPointer; Deref
+			func f11[S []rune, D *[2]rune | [2]rune | string](s S)          { _ = D(s) } // SliceToArrayPointer | SliceToArrayPointer; Deref | Convert
+			func f12[S []rune, D []rune](s S)                               { _ = D(s) } // ChangeType
+			func f13[S []rune, D []rune | string](s S)                      { _ = D(s) } // Convert | ChangeType
+			func f14[S []rune, D []rune | [2]rune](s S)                     { _ = D(s) } // ChangeType | SliceToArrayPointer; Deref
+			func f15[S []rune, D []rune | [2]rune | string](s S)            { _ = D(s) } // ChangeType | SliceToArrayPointer; Deref | Convert
+			func f16[S []rune, D []rune | *[2]rune](s S)                    { _ = D(s) } // ChangeType | SliceToArrayPointer
+			func f17[S []rune, D []rune | *[2]rune | string](s S)           { _ = D(s) } // ChangeType | SliceToArrayPointer | Convert
+			func f18[S []rune, D []rune | *[2]rune | [2]rune](s S)          { _ = D(s) } // ChangeType | SliceToArrayPointer | SliceToArrayPointer; Deref
+			func f19[S []rune, D []rune | *[2]rune | [2]rune | string](s S) { _ = D(s) } // ChangeType | SliceToArrayPointer | SliceToArrayPointer; Deref | Convert
+			func f20[S []rune | string, D string](s S)                      { _ = D(s) } // Convert | ChangeType
+			func f21[S []rune | string, D []rune](s S)                      { _ = D(s) } // Convert | ChangeType
+			func f22[S []rune | string, D []rune | string](s S)             { _ = D(s) } // ChangeType | Convert | Convert | ChangeType
+			func f23[S []rune | [2]rune, D [2]rune](s S)                    { _ = D(s) } // SliceToArrayPointer; Deref | ChangeType
+			func f24[S []rune | *[2]rune, D *[2]rune](s S)                  { _ = D(s) } // SliceToArrayPointer | ChangeType
+			`, nil,
+		},
+		{
+			"matching named and underlying types", `
+			package p
+			type a string
+			type b string
+			func g0[S []rune | a | b, D []rune | a | b](s S)      { _ = D(s) }
+			func g1[S []rune | ~string, D []rune | a | b](s S)    { _ = D(s) }
+			func g2[S []rune | a | b, D []rune | ~string](s S)    { _ = D(s) }
+			func g3[S []rune | ~string, D []rune |~string](s S)   { _ = D(s) }
+			`, nil,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			fset := token.NewFileSet()
+			f, err := parser.ParseFile(fset, "p.go", tc.src, 0)
+			if err != nil {
+				t.Error(err)
+			}
+			files := []*ast.File{f}
+
+			pkg := types.NewPackage("p", "")
+			conf := &types.Config{Importer: tc.importer}
+			_, _, err = ssautil.BuildPackage(conf, fset, pkg, files, ssa.SanityCheckFunctions)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+		})
+	}
 }
