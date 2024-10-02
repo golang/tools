0b989c812dcb662451db0b70a8267fe7e211886e
Tim King <taking@google.com>
2024-10-02 15:51:48 -0700

    internal/versions: update test expectations
    Updates test expecations now that go.dev/cl/607955 has been submitted.
    
    Change-Id: I795a14f7690026620f270e2db329945286764046
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/617635
    Reviewed-by: Michael Matloob <matloob@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Commit-Queue: Tim King <taking@google.com>

--- internal/versions/types_test.go
+++ internal/versions/types_test.go
@@ -11,51 +11,195 @@ import (
 	"go/parser"
 	"go/token"
 	"go/types"
+	"strings"
 	"testing"
 
+	"golang.org/x/tools/internal/testenv"
 	"golang.org/x/tools/internal/versions"
 )
 
+var contents = map[string]string{
+	"gobuild122.go": `
+//go:build go1.22
+package p
+`,
+	"gobuild121.go": `
+//go:build go1.21
+package p
+`,
+	"gobuild120.go": `
+//go:build go1.20
+package p
+`,
+	"gobuild119.go": `
+//go:build go1.19
+package p
+`,
+	"noversion.go": `
+package p
+`,
+}
+
 func Test(t *testing.T) {
-	var contents = map[string]string{
-		"gobuild.go": `
-	//go:build go1.23
-	package p
-	`,
-		"noversion.go": `
-	package p
-	`,
-	}
-	type fileTest struct {
-		fname string
-		want  string
-	}
+	testenv.NeedsGo1Point(t, 23) // TODO(#69749): Allow on 1.22 if a fix for #69749 is submitted.
+
 	for _, item := range []struct {
 		goversion string
 		pversion  string
 		tests     []fileTest
 	}{
-		//  {"", "", []fileTest{{"noversion.go", ""}, {"gobuild.go", ""}}}, // TODO(matloob): re-enable this test (with modifications) once CL 607955 has been submitted
-		{"go1.22", "go1.22", []fileTest{{"noversion.go", "go1.22"}, {"gobuild.go", "go1.23"}}},
+		{
+			"", "", []fileTest{
+				{"noversion.go", ""},
+				{"gobuild119.go", "go1.21"},
+				{"gobuild120.go", "go1.21"},
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+		{
+			"go1.20", "go1.20", []fileTest{
+				{"noversion.go", "go1.20"},
+				{"gobuild119.go", "go1.21"},
+				{"gobuild120.go", "go1.21"},
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+		{
+			"go1.21", "go1.21", []fileTest{
+				{"noversion.go", "go1.21"},
+				{"gobuild119.go", "go1.21"},
+				{"gobuild120.go", "go1.21"},
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+		{
+			"go1.22", "go1.22", []fileTest{
+				{"noversion.go", "go1.22"},
+				{"gobuild119.go", "go1.21"},
+				{"gobuild120.go", "go1.21"},
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
 	} {
 		name := fmt.Sprintf("types.Config{GoVersion:%q}", item.goversion)
 		t.Run(name, func(t *testing.T) {
+			testFiles(t, item.goversion, item.pversion, item.tests)
+		})
+	}
+}
+
+func TestToolchain122(t *testing.T) {
+	// TestToolchain122 tests the 1.22 toolchain for the FileVersion it returns.
+	// These results are at the moment unique to 1.22. So test it with distinct
+	// expectations.
+
+	// TODO(#69749): Remove requirement if a fix for #69749 is submitted.
+	if testenv.Go1Point() != 22 {
+		t.Skip("Expectations are only for 1.22 toolchain")
+	}
+
+	for _, item := range []struct {
+		goversion string
+		pversion  string
+		tests     []fileTest
+	}{
+		{
+			"", "", []fileTest{
+				{"noversion.go", ""},
+				{"gobuild119.go", ""},  // differs
+				{"gobuild120.go", ""},  // differs
+				{"gobuild121.go", ""},  // differs
+				{"gobuild122.go", ""}}, // differs
+		},
+		{
+			"go1.20", "go1.20", []fileTest{
+				{"noversion.go", "go1.20"},
+				{"gobuild119.go", "go1.20"}, // differs
+				{"gobuild120.go", "go1.20"}, // differs
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+		{
+			"go1.21", "go1.21", []fileTest{
+				{"noversion.go", "go1.21"},
+				{"gobuild119.go", "go1.19"}, // differs
+				{"gobuild120.go", "go1.20"}, // differs
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+		{
+			"go1.22", "go1.22", []fileTest{
+				{"noversion.go", "go1.22"},
+				{"gobuild119.go", "go1.19"}, // differs
+				{"gobuild120.go", "go1.20"}, // differs
+				{"gobuild121.go", "go1.21"},
+				{"gobuild122.go", "go1.22"}},
+		},
+	} {
+		name := fmt.Sprintf("types.Config{GoVersion:%q}", item.goversion)
+		t.Run(name, func(t *testing.T) {
+			testFiles(t, item.goversion, item.pversion, item.tests)
+		})
+	}
+}
+
+type fileTest struct {
+	fname string
+	want  string
+}
+
+func testFiles(t *testing.T, goversion string, pversion string, tests []fileTest) {
+
+	fset := token.NewFileSet()
+	files := make([]*ast.File, len(tests))
+	for i, test := range tests {
+		files[i] = parse(t, fset, test.fname, contents[test.fname])
+	}
+	pkg, info, err := typeCheck(fset, files, goversion)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got, want := pkg.GoVersion(), pversion; versions.Compare(got, want) != 0 {
+		t.Errorf("GoVersion()=%q. expected %q", got, want)
+	}
+	if got := versions.FileVersion(info, nil); got != "" {
+		t.Errorf(`FileVersions(nil)=%q. expected ""`, got)
+	}
+	for i, test := range tests {
+		if got, want := versions.FileVersion(info, files[i]), test.want; got != want {
+			t.Errorf("FileVersions(%s)=%q. expected %q", test.fname, got, want)
+		}
+	}
+}
+
+func TestTooNew(t *testing.T) {
+	testenv.NeedsGo1Point(t, 23) // TODO(#69749): Allow on 1.22 if a fix for #69749 is submitted.
+
+	const contents = `
+	//go:build go1.99
+	package p
+	`
+	type fileTest struct {
+		fname string
+		want  string
+	}
+
+	for _, goversion := range []string{
+		"",
+		"go1.22",
+	} {
+		name := fmt.Sprintf("types.Config{GoVersion:%q}", goversion)
+		t.Run(name, func(t *testing.T) {
 			fset := token.NewFileSet()
-			files := make([]*ast.File, len(item.tests))
-			for i, test := range item.tests {
-				files[i] = parse(t, fset, test.fname, contents[test.fname])
+			files := []*ast.File{parse(t, fset, "p.go", contents)}
+			_, _, err := typeCheck(fset, files, goversion)
+			if err == nil {
+				t.Fatal("Expected an error from a using a TooNew file version")
 			}
-			pkg, info := typeCheck(t, fset, files, item.goversion)
-			if got, want := pkg.GoVersion(), item.pversion; versions.Compare(got, want) != 0 {
-				t.Errorf("GoVersion()=%q. expected %q", got, want)
-			}
-			if got := versions.FileVersion(info, nil); got != "" {
-				t.Errorf(`FileVersions(nil)=%q. expected ""`, got)
-			}
-			for i, test := range item.tests {
-				if got, want := versions.FileVersion(info, files[i]), test.want; got != want {
-					t.Errorf("FileVersions(%s)=%q. expected %q", test.fname, got, want)
-				}
+			got := err.Error()
+			want := "file requires newer Go version go1.99"
+			if !strings.Contains(got, want) {
+				t.Errorf("Error message %q did not include %q", got, want)
 			}
 		})
 	}
@@ -69,7 +213,7 @@ func parse(t *testing.T, fset *token.FileSet, name, src string) *ast.File {
 	return file
 }
 
-func typeCheck(t *testing.T, fset *token.FileSet, files []*ast.File, goversion string) (*types.Package, *types.Info) {
+func typeCheck(fset *token.FileSet, files []*ast.File, goversion string) (*types.Package, *types.Info, error) {
 	conf := types.Config{
 		Importer:  importer.Default(),
 		GoVersion: goversion,
@@ -77,8 +221,5 @@ func typeCheck(t *testing.T, fset *token.FileSet, files []*ast.File, goversion s
 	info := types.Info{}
 	versions.InitFileVersions(&info)
 	pkg, err := conf.Check("", fset, files, &info)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return pkg, &info
+	return pkg, &info, err
 }
