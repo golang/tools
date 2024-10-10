50179f2225d65cd976b0d7ee584e087d7efd265a
Tim King <taking@google.com>
2024-10-10 16:49:59 +0000

    Revert "internal/aliases: add a function to conditionally enable aliases"
    This reverts commit f8f3c13ff3cad44c8cf62bea13bf846e9d7e8b5a.
    
    Reason for revert: https://go.dev/cl/619395 appears to be a better solution for the same problem.
    
    Change-Id: Ie3f290fca74b2cc2627484504b6db780f89b3bb4
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/619415
    Reviewed-by: Robert Findley <rfindley@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Commit-Queue: Tim King <taking@google.com>

--- internal/aliases/aliases.go
+++ internal/aliases/aliases.go
@@ -5,13 +5,8 @@
 package aliases
 
 import (
-	"go/build"
 	"go/token"
 	"go/types"
-	"os"
-	"slices"
-	"strings"
-	"sync"
 )
 
 // Package aliases defines backward compatible shims
@@ -40,53 +35,4 @@ func NewAlias(enabled bool, pos token.Pos, pkg *types.Package, name string, rhs 
 		panic("cannot create an alias with type parameters when gotypesalias is not enabled")
 	}
 	return types.NewTypeName(pos, pkg, name, rhs)
-}
-
-// ConditionallyEnableGoTypesAlias enables the gotypesalias GODEBUG setting if
-// * the version of go used to compile the program is between 1.23 and 1.26,
-// * gotypesalias are not already enabled, and
-// * gotypesalias is not set in GODEBUG already
-// exactly once. Otherwise it does nothing.
-//
-// Recommended usage is to do the following within a main package:
-//
-//	func init() { ConditionallyEnableGoTypesAlias() }
-//
-// within a module with go version 1.22 or in GOPATH mode.
-func ConditionallyEnableGoTypesAlias() { conditionallyEnableGoTypesAliasOnce() }
-
-var conditionallyEnableGoTypesAliasOnce = sync.OnceFunc(conditionallyEnableGoTypesAlias)
-
-func conditionallyEnableGoTypesAlias() {
-	// Let R be the version of go the program was compiled with. Then
-	// if R < 1.22, do nothing as gotypesalias is meaningless,
-	// if R == 1.22, do not turn on gotypesalias (latent bugs),
-	// if 1.23 <= R && R <= 1.26, turn on gotypesalias, and
-	// if R >= 1.27, this is a guaranteed no-op.
-
-	if !slices.Contains(build.Default.ReleaseTags, "go1.23") {
-		return // R < 1.23 (do nothing)
-	}
-	if slices.Contains(build.Default.ReleaseTags, "go1.27") {
-		return // R >= 1.27 (do nothing)
-	}
-	// 1.23 <= R <= 1.26
-	_, anyIsAlias := types.Universe.Lookup("any").Type().(*types.Alias)
-	if anyIsAlias {
-		return // gotypesalias are already enabled.
-	}
-
-	// Does GODEBUG have gotypesalias set already?
-	godebugs := strings.Split(os.Getenv("GODEBUG"), ",")
-	for _, p := range godebugs {
-		if strings.HasPrefix(strings.TrimSpace(p), "gotypesalias=") {
-			// gotypesalias is set in GODEBUG already.
-			// Do not override this setting.
-			return
-		}
-	}
-
-	// Enable gotypesalias.
-	godebugs = append(godebugs, "gotypesalias=1")
-	os.Setenv("GODEBUG", strings.Join(godebugs, ","))
 }
