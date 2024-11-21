1e0d4ee6a4771f70b0f2625329c97b4103b69d09
Alan Donovan <adonovan@google.com>
2024-11-21 14:14:16 -0500

    go/analysis/checker: disable Example on wasm
    packages.Load needs os.Pipe.
    
    Fixes golang/go#70502
    
    Change-Id: I4fd5c0a0a4e2d76fdda24d474e9f0465462d649a
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/630677
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Tim King <taking@google.com>

--- go/analysis/checker/example_test.go
+++ go/analysis/checker/example_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build !wasm
+
 // The example command demonstrates a simple go/packages-based
 // analysis driver program.
 package checker_test
