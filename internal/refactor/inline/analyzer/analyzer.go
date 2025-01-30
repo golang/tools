f912a4f263d5f6f973b2b212ad38b37eaad8fdee
Jonathan Amsterdam <jba@google.com>
2025-01-30 15:14:37 -0500

    internal/refactor/inline/analyzer: inline consts into local scopes
    Warn about inline directives on constants defined to be the
    predeclared iota.
    
    Allow inlining when iota is redefined to be an ordinary identifier.
    
    Do not inline if the inlined value refers to a different object.
    
    For golang/go#32816.
    
    Change-Id: I890b3c7dab595bb6d27ada459db5b91e2364ee13
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/646255
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- internal/refactor/inline/analyzer/analyzer.go
+++ internal/refactor/inline/analyzer/analyzer.go
@@ -53,86 +53,85 @@ func run(pass *analysis.Pass) (any, error) {
 	// and export a fact for each one.
 	inlinableFuncs := make(map[*types.Func]*inline.Callee) // memoization of fact import (nil => no fact)
 	inlinableConsts := make(map[*types.Const]*goFixInlineConstFact)
-	for _, file := range pass.Files {
-		for _, decl := range file.Decls {
-			switch decl := decl.(type) {
-			case *ast.FuncDecl:
-				if hasInlineDirective(decl.Doc) {
-					content, err := readFile(decl)
-					if err != nil {
-						pass.Reportf(decl.Doc.Pos(), "invalid inlining candidate: cannot read source file: %v", err)
-						continue
-					}
-					callee, err := inline.AnalyzeCallee(discard, pass.Fset, pass.Pkg, pass.TypesInfo, decl, content)
-					if err != nil {
-						pass.Reportf(decl.Doc.Pos(), "invalid inlining candidate: %v", err)
-						continue
-					}
-					fn := pass.TypesInfo.Defs[decl.Name].(*types.Func)
-					pass.ExportObjectFact(fn, &goFixInlineFuncFact{callee})
-					inlinableFuncs[fn] = callee
-				}
 
-			case *ast.GenDecl:
-				if decl.Tok != token.CONST {
-					continue
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{(*ast.FuncDecl)(nil), (*ast.GenDecl)(nil)}
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		switch decl := n.(type) {
+		case *ast.FuncDecl:
+			if hasInlineDirective(decl.Doc) {
+				content, err := readFile(decl)
+				if err != nil {
+					pass.Reportf(decl.Doc.Pos(), "invalid inlining candidate: cannot read source file: %v", err)
+					return
 				}
-				// Accept inline directives on the entire decl as well as individual specs.
-				declInline := hasInlineDirective(decl.Doc)
-				for _, spec := range decl.Specs {
-					spec := spec.(*ast.ValueSpec) // guaranteed by Tok == CONST
-					if declInline || hasInlineDirective(spec.Doc) {
-						for i, name := range spec.Names {
-							if i >= len(spec.Values) {
-								// Possible following an iota.
-								break
-							}
-							val := spec.Values[i]
-							var rhsID *ast.Ident
-							switch e := val.(type) {
-							case *ast.Ident:
-								if e.Name == "iota" {
-									continue
-								}
-								rhsID = e
-							case *ast.SelectorExpr:
-								rhsID = e.Sel
-							default:
-								pass.Reportf(val.Pos(), "invalid //go:fix inline directive: const value is not the name of another constant")
+				callee, err := inline.AnalyzeCallee(discard, pass.Fset, pass.Pkg, pass.TypesInfo, decl, content)
+				if err != nil {
+					pass.Reportf(decl.Doc.Pos(), "invalid inlining candidate: %v", err)
+					return
+				}
+				fn := pass.TypesInfo.Defs[decl.Name].(*types.Func)
+				pass.ExportObjectFact(fn, &goFixInlineFuncFact{callee})
+				inlinableFuncs[fn] = callee
+			}
+
+		case *ast.GenDecl:
+			if decl.Tok != token.CONST {
+				return
+			}
+			// Accept inline directives on the entire decl as well as individual specs.
+			declInline := hasInlineDirective(decl.Doc)
+			for _, spec := range decl.Specs {
+				spec := spec.(*ast.ValueSpec) // guaranteed by Tok == CONST
+				if declInline || hasInlineDirective(spec.Doc) {
+					for i, name := range spec.Names {
+						if i >= len(spec.Values) {
+							// Possible following an iota.
+							break
+						}
+						val := spec.Values[i]
+						var rhsID *ast.Ident
+						switch e := val.(type) {
+						case *ast.Ident:
+							// Constants defined with the predeclared iota cannot be inlined.
+							if pass.TypesInfo.Uses[e] == builtinIota {
+								pass.Reportf(val.Pos(), "invalid //go:fix inline directive: const value is iota")
 								continue
 							}
-							lhs := pass.TypesInfo.Defs[name].(*types.Const)
-							rhs := pass.TypesInfo.Uses[rhsID].(*types.Const) // must be so in a well-typed program
-							con := &goFixInlineConstFact{
-								RHSName:    rhs.Name(),
-								RHSPkgPath: rhs.Pkg().Path(),
-							}
-							inlinableConsts[lhs] = con
-							// Create a fact only if the LHS is exported and defined at top level.
-							// We create a fact even if the RHS is non-exported,
-							// so we can warn about uses in other packages.
-							if lhs.Exported() && typesinternal.IsPackageLevel(lhs) {
-								pass.ExportObjectFact(lhs, con)
-							}
+							rhsID = e
+						case *ast.SelectorExpr:
+							rhsID = e.Sel
+						default:
+							pass.Reportf(val.Pos(), "invalid //go:fix inline directive: const value is not the name of another constant")
+							continue
+						}
+						lhs := pass.TypesInfo.Defs[name].(*types.Const)
+						rhs := pass.TypesInfo.Uses[rhsID].(*types.Const) // must be so in a well-typed program
+						con := &goFixInlineConstFact{
+							RHSName:    rhs.Name(),
+							RHSPkgPath: rhs.Pkg().Path(),
+						}
+						if rhs.Pkg() == pass.Pkg {
+							con.rhsObj = rhs
+						}
+						inlinableConsts[lhs] = con
+						// Create a fact only if the LHS is exported and defined at top level.
+						// We create a fact even if the RHS is non-exported,
+						// so we can warn uses in other packages.
+						if lhs.Exported() && typesinternal.IsPackageLevel(lhs) {
+							pass.ExportObjectFact(lhs, con)
 						}
 					}
 				}
-				// TODO(jba): in user doc, warn that a comments within a spec, as in
-				//     const a,
-				//        //go:fix inline
-				//        b = 1, 2
-				// will go unnoticed.
-				// (They appear only in File.Comments, and it doesn't seem worthwhile to wade through those.)
 			}
 		}
-	}
+	})
 
 	// Pass 2. Inline each static call to an inlinable function,
 	// and each reference to an inlinable constant.
 	//
 	// TODO(adonovan):  handle multiple diffs that each add the same import.
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	nodeFilter := []ast.Node{
+	nodeFilter = []ast.Node{
 		(*ast.File)(nil),
 		(*ast.CallExpr)(nil),
 		(*ast.Ident)(nil),
@@ -218,7 +217,6 @@ func run(pass *analysis.Pass) (any, error) {
 			if con, ok := pass.TypesInfo.Uses[n].(*types.Const); ok {
 				incon, ok := inlinableConsts[con]
 				if !ok {
-					// TODO(jba): call ImportObjectFact.
 					var fact goFixInlineConstFact
 					if pass.ImportObjectFact(con, &fact) {
 						incon = &fact
@@ -228,9 +226,29 @@ func run(pass *analysis.Pass) (any, error) {
 				if incon == nil {
 					return // nope
 				}
+				//
 				// We have an identifier A here (n),
 				// and an inlinable "const A = B" elsewhere (incon).
-				// Suggest replacing A with B.
+				// Consider replacing A with B.
+				// Check that the expression we are inlining (B) means the same thing
+				// (refers to the same object) in n's scope as it does in A's scope.
+				if incon.rhsObj != nil {
+					// Both expressions are in the current package.
+					// incon.rhsObj is the object referred to by B in the definition of A.
+					scope := pass.TypesInfo.Scopes[currentFile].Innermost(n.Pos()) // n's scope
+					_, obj := scope.LookupParent(incon.RHSName, n.Pos())           // what "B" means in n's scope
+					if obj == nil {
+						// Should be impossible: if code at n can refer to the LHS,
+						// it can refer to the RHS.
+						panic(fmt.Sprintf("no object for inlinable const %s RHS %s", n.Name, incon.RHSName))
+					}
+					if obj != incon.rhsObj {
+						// "B" means something different here than at the inlinable const's scope
+						return
+					}
+				} else {
+					// TODO(jba): handle the cross-package case by checking the package ID.
+				}
 				importPrefix := ""
 				if incon.RHSPkgPath != con.Pkg().Path() {
 					importID := maybeAddImportPath(currentFile, incon.RHSPkgPath)
@@ -266,8 +284,7 @@ func hasInlineDirective(cg *ast.CommentGroup) bool {
 }
 
 func maybeAddImportPath(f *ast.File, path string) string {
-	// TODO(jba): implement this in terms of existing functions.
-	// TODO(adonovan): tell jba which functions.
+	// TODO(jba): implement this in terms of analysisinternal.AddImport(info, file, pos, path, localname).
 	return "unimp"
 }
 
@@ -278,4 +295,21 @@ type goFixInlineFuncFact struct{ Callee *inline.Callee }
 func (f *goFixInlineFuncFact) String() string { return "goFixInline " + f.Callee.String() }
 func (*goFixInlineFuncFact) AFact()           {}
 
+// A goFixInlineConstFact is exported for each constant marked "//go:fix inline".
+// It holds information about an inlinable constant. Gob-serializable.
+type goFixInlineConstFact struct {
+	// Information about "const LHSName = RHSName".
+	RHSName    string
+	RHSPkgPath string
+	rhsObj     types.Object // for current package
+}
+
+func (c *goFixInlineConstFact) String() string {
+	return fmt.Sprintf("goFixInline const %q.%s", c.RHSPkgPath, c.RHSName)
+}
+
+func (*goFixInlineConstFact) AFact() {}
+
 func discard(string, ...any) {}
+
+var builtinIota = types.Universe.Lookup("iota")
