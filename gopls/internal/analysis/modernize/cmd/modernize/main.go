1320197d6c7ed6da48496e5e311c7ee76701e035
Alan Donovan <adonovan@google.com>
2025-02-06 14:29:43 -0500

    gopls/internal/analysis/modernize/cmd/modernize: create
    By moving the main.go file, gopls users will be able to run
    
    $ go run golang.org/x/tools/gopls/internal/analysis/modernize/cmd/modernize@latest -fix
    
    after the gopls release to apply all modernizer fixes en masse.
    
    Updates golang/go#70815
    
    Change-Id: I25352b7b77653c177310dfea7a050741949890f9
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/647355
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- gopls/internal/analysis/modernize/cmd/modernize/main.go
+++ gopls/internal/analysis/modernize/cmd/modernize/main.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build ignore
-
 // The modernize command suggests (or, with -fix, applies) fixes that
 // clarify Go code by using more modern features.
 package main
