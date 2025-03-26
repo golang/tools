golang/tools

commit 9abefc5f9e63648ff7076e2483dae802a82142cf

gopls/internal/analysis/modernize: permit int/uint type variants in rangeint
This CL tracks the todo and enhances the rangeint modernizer to provide fixes for all int/uint variants, such as 'for i := int32(0); ...'.

It now supports all int/uint variants when defining a new index variable with a zero value, including:

- int8, int16, int32, int64
- uint, uint8, uint16, uint32, uint64

In addition to the newly supported variants, this CL also handles zero float literals explicitly cast to integer/unsigned integer types, such as:

- 'for i := int32(0.); ...'
- 'for i := int32(.0); ...'

Change-Id: I1058d78eb6d3cbd8318c8c7e0d6b951ef0a5648c
Reviewed-on: https://go-review.googlesource.com/c/tools/+/659155
Auto-Submit: Alan Donovan <adonovan@google.com>
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: Robert Findley <rfindley@google.com>
Reviewed-by: Alan Donovan <adonovan@google.com>

gopls/internal/analysis/modernize/rangeint.go
@@ -31,8 +31,6 @@ import (
 //   - The ':=' may be replaced by '='.
 //   - The fix may remove "i :=" if it would become unused.
 //
-// TODO(adonovan): permit variants such as "i := int64(0)".
-//
 // Restrictions:
 //   - The variable i must not be assigned or address-taken within the
 //     loop, because a "for range int" loop does not respect assignments
@@ -54,7 +52,7 @@ func rangeint(pass *analysis.Pass) {
 			if init, ok := loop.Init.(*ast.AssignStmt); ok &&
 				isSimpleAssign(init) &&
 				is[*ast.Ident](init.Lhs[0]) &&
-				isZeroLiteral(init.Rhs[0]) {
+				isZeroIntLiteral(info, init.Rhs[0]) {
 				// Have: for i = 0; ... (or i := 0)
 				index := init.Lhs[0].(*ast.Ident)
 
@@ -145,11 +143,19 @@ func rangeint(pass *analysis.Pass) {
 						// re-type check the expression to detect this case.
 						var beforeLimit, afterLimit string
 						if v := info.Types[limit].Value; v != nil {
-							beforeLimit, afterLimit = "int(", ")"
+							tVar := info.TypeOf(init.Rhs[0])
+
+							// TODO(adonovan): use a types.Qualifier that respects the existing
+							// imports of this file that are visible (not shadowed) at the current position,
+							// and adds new imports as needed, similar to analysisinternal.AddImport.
+							// (Unfortunately types.Qualifier doesn't provide the name of the package
+							// member to be qualified, a qualifier cannot perform the necessary shadowing
+							// check for dot-imported names.)
+							beforeLimit, afterLimit = fmt.Sprintf("%s(", types.TypeString(tVar, (*types.Package).Name)), ")"
 							info2 := &types.Info{Types: make(map[ast.Expr]types.TypeAndValue)}
 							if types.CheckExpr(pass.Fset, pass.Pkg, limit.Pos(), limit, info2) == nil {
 								tLimit := types.Default(info2.TypeOf(limit))
-								if types.AssignableTo(tLimit, types.Typ[types.Int]) {
+								if types.AssignableTo(tLimit, tVar) {
 									beforeLimit, afterLimit = "", ""
 								}
 							}

gopls/internal/analysis/modernize/testdata/src/rangeint/rangeint.go
@@ -1,9 +1,51 @@
 package rangeint
 
+import (
+	"os"
+	os1 "os"
+)
+
 func _(i int, s struct{ i int }, slice []int) {
 	for i := 0; i < 10; i++ { // want "for loop can be modernized using range over int"
 		println(i)
 	}
+	for j := int(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int8(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int16(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int32(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int64(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := uint8(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := uint16(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := uint32(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := uint64(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int8(0.); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := int8(.0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := os.FileMode(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+
 	{
 		var i int
 		for i = 0; i < f(); i++ { // want "for loop can be modernized using range over int"
@@ -21,6 +63,12 @@ func _(i int, s struct{ i int }, slice []int) {
 	}
 
 	// nope
+	for j := .0; j < 10; j++ { // nope: j is a float type
+		println(j)
+	}
+	for j := float64(0); j < 10; j++ { // nope: j is a float type
+		println(j)
+	}
 	for i := 0; i < 10; { // nope: missing increment
 	}
 	for i := 0; i < 10; i-- { // nope: negative increment
@@ -72,6 +120,10 @@ func issue71847d() {
 	const limit = 1e3            // float
 	for i := 0; i < limit; i++ { // want "for loop can be modernized using range over int"
 	}
+	for i := int(0); i < limit; i++ { // want "for loop can be modernized using range over int"
+	}
+	for i := uint(0); i < limit; i++ { // want "for loop can be modernized using range over int"
+	}
 
 	const limit2 = 1 + 0i         // complex
 	for i := 0; i < limit2; i++ { // want "for loop can be modernized using range over int"
@@ -96,5 +148,11 @@ func issue72726() {
 
 	var arr []int
 	for i = 0; i < arr[i]; i++ { // nope
+	}
+}
+
+func todo() {
+	for j := os1.FileMode(0); j < 10; j++ { // want "for loop can be modernized using range over int"
+		println(j)
 	}
 }

gopls/internal/analysis/modernize/testdata/src/rangeint/rangeint.go.golden
@@ -1,9 +1,51 @@
 package rangeint
 
+import (
+	"os"
+	os1 "os"
+)
+
 func _(i int, s struct{ i int }, slice []int) {
 	for i := range 10 { // want "for loop can be modernized using range over int"
 		println(i)
 	}
+	for j := range 10 { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int8(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int16(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int32(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int64(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range uint8(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range uint16(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range uint32(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range uint64(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int8(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range int8(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+	for j := range os.FileMode(10) { // want "for loop can be modernized using range over int"
+		println(j)
+	}
+
 	{
 		var i int
 		for i = range f() { // want "for loop can be modernized using range over int"
@@ -21,6 +63,12 @@ func _(i int, s struct{ i int }, slice []int) {
 	}
 
 	// nope
+	for j := .0; j < 10; j++ { // nope: j is a float type
+		println(j)
+	}
+	for j := float64(0); j < 10; j++ { // nope: j is a float type
+		println(j)
+	}
 	for i := 0; i < 10; { // nope: missing increment
 	}
 	for i := 0; i < 10; i-- { // nope: negative increment
@@ -72,6 +120,10 @@ func issue71847d() {
 	const limit = 1e3            // float
 	for range int(limit) { // want "for loop can be modernized using range over int"
 	}
+	for range int(limit) { // want "for loop can be modernized using range over int"
+	}
+	for range uint(limit) { // want "for loop can be modernized using range over int"
+	}
 
 	const limit2 = 1 + 0i         // complex
 	for range int(limit2) { // want "for loop can be modernized using range over int"
@@ -96,5 +148,11 @@ func issue72726() {
 
 	var arr []int
 	for i = 0; i < arr[i]; i++ { // nope
+	}
+}
+
+func todo() {
+	for j := range os.FileMode(10)  { // want "for loop can be modernized using range over int"
+		println(j)
 	}
 }

gopls/internal/analysis/modernize/bloop.go
@@ -101,7 +101,7 @@ func bloop(pass *analysis.Pass) {
 						if assign, ok := n.Init.(*ast.AssignStmt); ok &&
 							assign.Tok == token.DEFINE &&
 							len(assign.Rhs) == 1 &&
-							isZeroLiteral(assign.Rhs[0]) &&
+							isZeroIntLiteral(info, assign.Rhs[0]) &&
 							is[*ast.IncDecStmt](n.Post) &&
 							n.Post.(*ast.IncDecStmt).Tok == token.INC &&
 							equalSyntax(n.Post.(*ast.IncDecStmt).X, assign.Lhs[0]) &&

gopls/internal/analysis/modernize/modernize.go
@@ -7,6 +7,7 @@ package modernize
 import (
 	_ "embed"
 	"go/ast"
+	"go/constant"
 	"go/format"
 	"go/token"
 	"go/types"
@@ -117,10 +118,9 @@ func formatExprs(fset *token.FileSet, exprs []ast.Expr) string {
 	return buf.String()
 }
 
-// isZeroLiteral reports whether e is the literal 0.
-func isZeroLiteral(e ast.Expr) bool {
-	lit, ok := e.(*ast.BasicLit)
-	return ok && lit.Kind == token.INT && lit.Value == "0"
+// isZeroIntLiteral reports whether e is an integer whose value is 0.
+func isZeroIntLiteral(info *types.Info, e ast.Expr) bool {
+	return info.Types[e].Value == constant.MakeInt64(0)
 }
 
 // filesUsing returns a cursor for each *ast.File in the inspector

gopls/internal/analysis/modernize/slices.go
@@ -216,7 +216,7 @@ func clippedSlice(info *types.Info, e ast.Expr) (res ast.Expr, empty bool) {
 		// x[:0:0], x[:len(x):len(x)], x[:k:k]
 		if e.Slice3 && e.High != nil && e.Max != nil && equalSyntax(e.High, e.Max) { // x[:k:k]
 			res = e
-			empty = isZeroLiteral(e.High) // x[:0:0]
+			empty = isZeroIntLiteral(info, e.High) // x[:0:0]
 			if call, ok := e.High.(*ast.CallExpr); ok &&
 				typeutil.Callee(info, call) == builtinLen &&
 				equalSyntax(call.Args[0], e.X) {

commit 8c42f8aeff807484822b4be1294b82696f8fc17e

gopls/internal/analysis/modernize: use types.RelativeTo to respect current package
This CL fixes a bug introduced in CL659155. It leverages types.RelativeTo
to determine the package name of a variable, ensuring that the package
name is not added for types declared within the same package. This prevents
invalid type errors.

Additionally, this CL introduces additional test cases.

Fixes golang/go#73037

Change-Id: I6ad0c80c02ad1a679a956b93a53a05a8eb1ba9c2
Reviewed-on: https://go-review.googlesource.com/c/tools/+/660815
LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
Auto-Submit: Alan Donovan <adonovan@google.com>
Reviewed-by: Alan Donovan <adonovan@google.com>

gopls/internal/analysis/modernize/rangeint.go
@@ -184,7 +184,7 @@ func rangeint(pass *analysis.Pass) {
 							// (Unfortunately types.Qualifier doesn't provide the name of the package
 							// member to be qualified, a qualifier cannot perform the necessary shadowing
 							// check for dot-imported names.)
-							beforeLimit, afterLimit = fmt.Sprintf("%s(", types.TypeString(tVar, (*types.Package).Name)), ")"
+							beforeLimit, afterLimit = fmt.Sprintf("%s(", types.TypeString(tVar, types.RelativeTo(pass.Pkg))), ")"
 							info2 := &types.Info{Types: make(map[ast.Expr]types.TypeAndValue)}
 							if types.CheckExpr(pass.Fset, pass.Pkg, limit.Pos(), limit, info2) == nil {
 								tLimit := types.Default(info2.TypeOf(limit))

gopls/internal/analysis/modernize/testdata/src/rangeint/rangeint.go
@@ -205,3 +205,29 @@ func todo() {
 		println(j)
 	}
 }
+
+type T uint
+type TAlias = uint
+
+func Fn(a int) T {
+	return T(a)
+}
+
+func issue73037() {
+	var q T
+	for a := T(0); a < q; a++ { // want "for loop can be modernized using range over int"
+		println(a)
+	}
+	for a := Fn(0); a < q; a++ {
+		println(a)
+	}
+	var qa TAlias
+	for a := TAlias(0); a < qa; a++ { // want "for loop can be modernized using range over int"
+		println(a)
+	}
+	for a := T(0); a < 10; a++ { // want "for loop can be modernized using range over int"
+		for b := T(0); b < 10; b++ { // want "for loop can be modernized using range over int"
+			println(a, b)
+		}
+	}
+}

gopls/internal/analysis/modernize/testdata/src/rangeint/rangeint.go.golden
@@ -205,3 +205,29 @@ func todo() {
 		println(j)
 	}
 }
+
+type T uint
+type TAlias = uint
+
+func Fn(a int) T {
+	return T(a)
+}
+
+func issue73037() {
+    var q T
+	for a := range q { // want "for loop can be modernized using range over int"
+	    println(a)
+	}
+	for a := Fn(0); a < q; a++ {
+		println(a)
+	}
+    var qa TAlias
+    for a := range qa { // want "for loop can be modernized using range over int"
+        println(a)
+    }
+	for a := range T(10) { // want "for loop can be modernized using range over int"
+		for b := range T(10) { // want "for loop can be modernized using range over int"
+			println(a, b)
+		}
+	}
+}