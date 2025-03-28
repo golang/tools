aac3cf020c8adcf240806fba32b816bc55214ea5
Jonathan Amsterdam <jba@google.com>
2025-03-28 11:16:53 -0400

    internal/refactor/inline: improve freeishNames
    Perform a more sophisticated free-name analysis.
    
    Adapt the name resolution logic from go/parser to find free names
    in an ast.Node.
    
    As the function doc says, the new function is stymied only by
    the composite-lit ambiguity and thus significantly more accurate
    that the old implementation, which did not take into account
    bindings within expressions.
    
    The test provides full coverage, except for some uninteresting edge
    cases.
    
    Change-Id: Id1e18b5e9b1aae78feb2066f5243fd565568ffdd
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/661337
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>

---
+++ internal/refactor/inline/free_test.go
@@ -0,0 +1,206 @@
+// Copyright 2025 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package inline
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"maps"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestFreeishNames(t *testing.T) {
+	elems := func(m map[string]bool) string {
+		return strings.Join(slices.Sorted(maps.Keys(m)), " ")
+	}
+
+	for _, test := range []struct {
+		code string // one or more exprs, decls or stmts
+		want string // space-separated list of free names
+	}{
+		{
+			`x`,
+			"x",
+		},
+		{
+			`x.y.z`,
+			"x",
+		},
+		{
+			`T{a: 1, b: 2, c.d: e}`,
+			"a b c e T",
+		},
+		{
+			`f(x)`,
+			"f x",
+		},
+		{
+			`f.m(x)`,
+			"f x",
+		},
+		{
+			`func(x int) int { return x + y }`,
+			"int y",
+		},
+		{
+			`x = func(x int) int { return 2*x }()`,
+			"int x",
+		},
+		{
+			`func(x int) (y int) { return x + y }`,
+			"int",
+		},
+		{
+			`struct{a **int; b map[int][]bool}`,
+			"bool int",
+		},
+		{
+			`struct{f int}{f: 0}`,
+			"f int",
+		},
+		{
+			`interface{m1(int) bool; m2(x int) (y bool)}`,
+			"bool int",
+		},
+		{
+			`x := 1; x++`,
+			"",
+		},
+		{
+			`x = 1`,
+			"x",
+		},
+		{
+			`_ = 1`,
+			"",
+		},
+		{
+			`x, y := 1, 2; x = y + z`,
+			"z",
+		},
+		{
+			`x, y := y, x; x = y + z`,
+			"x y z",
+		},
+		{
+			`a, b := 0, 0; b, c := 0, 0; print(a, b, c, d)`,
+			"d print",
+		},
+		{
+			`label: x++`,
+			"x",
+		},
+		{
+			`if x == y {x}`,
+			"x y",
+		},
+		{
+			`if x := 1; x == y {x}`,
+			"y",
+		},
+		{
+			`if x := 1; x == y {x} else {z}`,
+			"y z",
+		},
+		{
+			`switch x { case 1: x; case y: z }`,
+			"x y z",
+		},
+		{
+			`switch x := 1; x { case 1: x; case y: z }`,
+			"y z",
+		},
+		{
+			`switch x.(type) { case int: x; case []int: y }`,
+			"int x y",
+		},
+		{
+			`switch x := 1; x.(type) { case int: x; case []int: y }`,
+			"int y",
+		},
+		{
+			`switch y := x.(type) { case int: x; case []int: y }`,
+			"int x",
+		},
+		{
+			`select { case c <- 1: x; case x := <-c: 2; default: y}`,
+			"c x y",
+		},
+		{
+			`for i := 0; i < 9; i++ { c <- j }`,
+			"c j",
+		},
+		{
+			`for i = 0; i < 9; i++ { c <- j }`,
+			"c i j",
+		},
+		{
+			`for i := range 9 { c <- j }`,
+			"c j",
+		},
+		{
+			`for i = range 9 { c <- j }`,
+			"c i j",
+		},
+		{
+			`for _, e := range []int{1, 2, x} {e}`,
+			"int x",
+		},
+		{
+			`var x, y int; f(x, y)`,
+			"f int",
+		},
+		{
+			`{var x, y int}; f(x, y)`,
+			"f int x y",
+		},
+		{
+			`const x = 1; { const y = iota; return x, y }`,
+			"iota",
+		},
+		{
+			`type t int; t(0)`,
+			"int",
+		},
+		{
+			`type t[T ~int] struct { t T };  x = t{t: 1}.t`, // field t shadowed by type decl
+			"int x",
+		},
+		{
+			`type t[S ~[]E, E any] S`,
+			"any",
+		},
+		{
+			`var a [unsafe.Sizeof(func(x int) { x + y })]int`,
+			"int unsafe y",
+		},
+	} {
+		_, f := mustParse(t, "free.go", `package p; func _() {`+test.code+`}`)
+		n := f.Decls[0].(*ast.FuncDecl).Body
+		got := map[string]bool{}
+		want := map[string]bool{}
+		for _, n := range strings.Fields(test.want) {
+			want[n] = true
+		}
+
+		freeishNames(got, n)
+
+		if !maps.Equal(got, want) {
+			t.Errorf("\ncode  %s\ngot   %v\nwant  %v", test.code, elems(got), elems(want))
+		}
+	}
+}
+
+func mustParse(t *testing.T, filename string, content any) (*token.FileSet, *ast.File) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filename, content, parser.ParseComments|parser.SkipObjectResolution)
+	if err != nil {
+		t.Fatalf("ParseFile: %v", err)
+	}
+	return fset, f
+}
--- internal/refactor/inline/inline.go
+++ internal/refactor/inline/inline.go
@@ -2448,41 +2448,6 @@ func freeVars(info *types.Info, e ast.Expr) map[string]bool {
 	return free
 }
 
-// freeishNames computes an over-approximation to the free names
-// of the expression (type or term) t, inserting values into the map.
-//
-// If t is a type expression, the approximation is not too far off (see below). For
-// terms, it simply gathers all unqualified identifiers, ignoring scopes established
-// by function and composite literals, so in some cases it can over-estimate quite
-// a lot.
-//
-// Because we don't have go/types annotations, we can't give an exact
-// result in all cases. In particular, an array type [n]T might have a
-// size such as unsafe.Sizeof(func() int{stmts...}()) and now the
-// precise answer depends upon all the statement syntax too. But that
-// never happens in practice.
-func freeishNames(free map[string]bool, t ast.Expr) {
-	var visit func(n ast.Node) bool
-	visit = func(n ast.Node) bool {
-		switch n := n.(type) {
-		case *ast.Ident:
-			free[n.Name] = true
-
-		case *ast.SelectorExpr:
-			ast.Inspect(n.X, visit)
-			return false // don't visit .Sel
-
-		case *ast.Field:
-			// Visit Type (which may have free references)
-			// but not Names (which are defs, not uses).
-			ast.Inspect(n.Type, visit)
-			return false
-		}
-		return true
-	}
-	ast.Inspect(t, visit)
-}
-
 // effects reports whether an expression might change the state of the
 // program (through function calls and channel receives) and affect
 // the evaluation of subsequent expressions.
