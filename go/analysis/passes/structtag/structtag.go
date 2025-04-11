7ceff1355d9d7ec7f8404fb670ac112a46681ff6
Damien Neil <dneil@google.com>
2025-04-11 14:31:32 -0700

    go/analysis/passes/structtag: ignore findings for "encoding/..."
    Don't warn on structtag findings for any of the new packages
    added as part of the json v2 project.
    
    Just skip all of "encoding/..." on the theory that if anything
    in std is using invalid tags, it has a good reason.
    
    Change-Id: Ie9fbbe9ef1be7ea574c74824a26951501f6c5e2d
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/665055
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Dmitri Shuralyov <dmitshur@golang.org>
    Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>

--- go/analysis/passes/structtag/structtag.go
+++ go/analysis/passes/structtag/structtag.go
@@ -89,8 +89,7 @@ var checkTagSpaces = map[string]bool{"json": true, "xml": true, "asn1": true}
 
 // checkCanonicalFieldTag checks a single struct field tag.
 func checkCanonicalFieldTag(pass *analysis.Pass, field *types.Var, tag string, seen *namesSeen) {
-	switch pass.Pkg.Path() {
-	case "encoding/json", "encoding/json/v2", "encoding/xml":
+	if strings.HasPrefix(pass.Pkg.Path(), "encoding/") {
 		// These packages know how to use their own APIs.
 		// Sometimes they are testing what happens to incorrect programs.
 		return
