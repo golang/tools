cb39a5f00b69f514f37bc2a838174cc12c38c112
Alan Donovan <adonovan@google.com>
2025-06-04 10:56:09 -0400

    gopls/internal/golang: Format generated files
    This CL reverts the effect of CL 365295 (to address issue golang/go#49555),
    which caused Format to fail on generated files. We believe this
    was a mistake: Go developers have plenty of reasons to temporarily
    edit generated files, for example to quickly experiment with changes
    without having to rewrite the code generator, or to add a log.Print
    statement while debugging. If a client asks gopls to format a file
    or organize its imports, gopls should do that, even if the file
    contains a "generated..." comment.
    
    + test
    
    Updates golang/go#49555
    Fixes golang/go#73959
    
    Change-Id: I2d02d5b2611b1599068df8fc267453773c66e027
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/678815
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Alan Donovan <adonovan@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- gopls/internal/golang/format.go
+++ gopls/internal/golang/format.go
@@ -40,11 +40,6 @@ func Format(ctx context.Context, snapshot *cache.Snapshot, fh file.Handle) ([]pr
 		return nil, err
 	}
 
-	// Generated files shouldn't be edited. So, don't format them.
-	if ast.IsGenerated(pgf.File) {
-		return nil, fmt.Errorf("can't format %q: file is generated", fh.URI().Path())
-	}
-
 	// Even if this file has parse errors, it might still be possible to format it.
 	// Using format.Node on an AST with errors may result in code being modified.
 	// Attempt to format the source of this file instead.
