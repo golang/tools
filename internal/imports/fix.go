88c2f118474f794885a550e74931cbefb06dd6d0
Kir Kolyshkin <kolyshkin@gmail.com>
2025-09-10 15:03:40 -0700

    internal/imports: remove unused field
    This was never really used anywhere, only initialized in
    (*gopathResolver).ClearForNewScan, which was removed by CL 561235.
    
    While at it, fix imports formatting (done by gofmt).
    
    Change-Id: I3b060c94822ac72362eb696ed2d85b35378a5586
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/702655
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Commit-Queue: Alan Donovan <adonovan@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    Auto-Submit: Alan Donovan <adonovan@google.com>
    Auto-Submit: Kirill Kolyshkin <kolyshkin@gmail.com>

--- internal/imports/fix.go
+++ internal/imports/fix.go
@@ -16,6 +16,7 @@ import (
 	"go/types"
 	"io/fs"
 	"io/ioutil"
+	"maps"
 	"os"
 	"path"
 	"path/filepath"
@@ -26,8 +27,6 @@ import (
 	"sync"
 	"unicode"
 	"unicode/utf8"
-
-	"maps"
 
 	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/internal/event"
@@ -1251,7 +1250,6 @@ func ImportPathToAssumedName(importPath string) string {
 // gopathResolver implements resolver for GOPATH workspaces.
 type gopathResolver struct {
 	env      *ProcessEnv
-	walked   bool
 	cache    *DirInfoCache
 	scanSema chan struct{} // scanSema prevents concurrent scans.
 }
