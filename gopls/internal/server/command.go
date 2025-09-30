371f6399a1c8e6be241d179f701ec96eb3965393
Ethan Lee <ethanalee@google.com>
2025-09-30 14:59:26 +0000

    gopls/internal/server: fix typo in RunGovulncheck
    - We are deduplicating, not debouncing.
    
    Change-Id: I18b32789857fa7fd65462e00cf1843359310b162
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/707995
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Ethan Lee <ethanalee@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>

--- gopls/internal/server/command.go
+++ gopls/internal/server/command.go
@@ -1290,7 +1290,7 @@ func (c *commandHandler) Vulncheck(ctx context.Context, args command.VulncheckAr
 // TODO(golang/vscode-go#3572)
 // TODO(hxjiang): deprecate gopls.run_govulncheck.
 func (c *commandHandler) RunGovulncheck(ctx context.Context, args command.VulncheckArgs) (command.RunVulncheckResult, error) {
-	// Debounce the RunGovulncheck command so only one is running at a time.
+	// Deduplicate the RunGovulncheck command so only one is running at a time.
 	if !c.s.runGovulncheckInProgress.CompareAndSwap(false, true) {
 		c.s.client.ShowMessage(ctx, &protocol.ShowMessageParams{
 			Type:    protocol.Info,
