77c16e61c57745a0c5d3b6af058567efbf2af7f1
Lin Lin <linlin152@foxmail.com>
2025-12-06 09:32:34 +0800

    internal: fix unused errors reported by ineffassign
    Updates #76704

--- internal/goroot/importcfg.go
+++ internal/goroot/importcfg.go
@@ -16,37 +16,31 @@ import (
 	"sync"
 )
 
-var (
-	stdlibPkgfileMap map[string]string
-	stdlibPkgfileErr error
-	once             sync.Once
-)
-
 // PkgfileMap returns a map of package paths to the location on disk
 // of the .a file for the package.
 // The caller must not modify the map.
 func PkgfileMap() (map[string]string, error) {
-	once.Do(func() {
-		m := make(map[string]string)
-		output, err := exec.Command("go", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
-		if err != nil {
-			stdlibPkgfileErr = err
-		}
-		for line := range strings.SplitSeq(string(output), "\n") {
-			if line == "" {
-				continue
-			}
-			sp := strings.SplitN(line, " ", 2)
-			if len(sp) != 2 {
-				err = fmt.Errorf("determining pkgfile map: invalid line in go list output: %q", line)
-				return
-			}
-			importPath, export := sp[0], sp[1]
-			if export != "" {
-				m[importPath] = export
-			}
-		}
-		stdlibPkgfileMap = m
-	})
-	return stdlibPkgfileMap, stdlibPkgfileErr
+	return pkgFileMapOnce()
 }
+
+var pkgFileMapOnce = sync.OnceValues(func() (map[string]string, error) {
+	m := make(map[string]string)
+	output, err := exec.Command("go", "list", "-export", "-e", "-f", "{{.ImportPath}} {{.Export}}", "std", "cmd").Output()
+	if err != nil {
+		return m, err
+	}
+	for line := range strings.SplitSeq(string(output), "\n") {
+		if line == "" {
+			continue
+		}
+		sp := strings.SplitN(line, " ", 2)
+		if len(sp) != 2 {
+			return m, fmt.Errorf("determining pkgfile map: invalid line in go list output: %q", line)
+		}
+		importPath, export := sp[0], sp[1]
+		if export != "" {
+			m[importPath] = export
+		}
+	}
+	return m, nil
+})
