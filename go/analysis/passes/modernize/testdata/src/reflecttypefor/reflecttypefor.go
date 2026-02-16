9ab7c532fcecb0ed695cf64f85a1103f5e6b4e54
Konnyaku <beifengxuanxiao@126.com>
2026-02-17 04:24:29 +0800

    [internal-branch.go1.26-vendor] go/analysis/passes/modernize: fix invalid rewrite of reflect.TypeOf(nil)
    The modernization pass previously incorrectly transformed reflect.TypeOf(nil)
    into reflect.TypeFor[nil](), which is invalid Go code because nil is untyped
    and cannot be used as a type argument.
    
    This change adds a check to skip the rewrite when the argument is an untyped nil.
    
    Updates golang/go#77803
    
    Change-Id: I1f93caf9a1bfc10ce5ed1e31edc7a4aaf08490de
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/746060
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Mark Freeman <markfreeman@google.com>
    Reviewed-by: Madeline Kalil <mkalil@google.com>
    (cherry picked from commit 67b6d4f72be7bec91efe4768b63549603d6483d9)
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/749501
    Reviewed-by: Cherry Mui <cherryyz@google.com>

--- go/analysis/passes/modernize/testdata/src/reflecttypefor/reflecttypefor.go
+++ go/analysis/passes/modernize/testdata/src/reflecttypefor/reflecttypefor.go
@@ -10,6 +10,7 @@ var (
 	x any
 	_ = reflect.TypeOf(x)                 // nope (dynamic)
 	_ = reflect.TypeOf(0)                 // want "reflect.TypeOf call can be simplified using TypeFor"
+	_ = reflect.TypeOf(nil)               // nope (likely a mistake)
 	_ = reflect.TypeOf(uint(0))           // want "reflect.TypeOf call can be simplified using TypeFor"
 	_ = reflect.TypeOf(error(nil))        // nope (likely a mistake)
 	_ = reflect.TypeOf((*error)(nil))     // want "reflect.TypeOf call can be simplified using TypeFor"
@@ -22,6 +23,11 @@ var (
 
 // Eliminate local var if we deleted its last use.
 func _() {
+	// Test for shadowed nil
+	nil := "nil"
+	_ = reflect.TypeOf(nil) // want "reflect.TypeOf call can be simplified using TypeFor"
+	_ = nil                 // shadowed nil has multiple uses
+
 	var zero string
 	_ = reflect.TypeOf(zero) // want "reflect.TypeOf call can be simplified using TypeFor"
 
