83e333ae1f051d6596720d80df668c8dfc4e6f54
Madeline Kalil <mkalil@google.com>
2026-05-01 16:47:02 -0400

    gopls/internal/golang: fix invalid comment on hover
    A previous CL (go.dev/cl/761080) modified the hover
    logic for assign statements to include the comment
    immediately preceeding the assignment.
    
    However, it used slices.BinarySearchFunc to find
    the preceeding comment without checking the
    "found" returned value.
    
    slices.BinarySearchFunc can return a valid
    index value even when it didn't find the target comment.
    If we use the index returned without checking
    whether found=true, we end up including an invalid comment
    in the hover information; i.e. a comment from a different
    syntax node.
    
    For some unknown reason I could not produce this error
    in the marker tests, I had to include the regression test
    in test/integration/hover.
    
    Fixes golang/go#79146
    Change-Id: Ib1d2341f628475d732b8ef53c6dd0095d7c7ad29
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/773221
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- gopls/internal/test/integration/hover/hover_test.go
+++ gopls/internal/test/integration/hover/hover_test.go
@@ -6,6 +6,7 @@ package hover
 
 import (
 	"os"
+	"strings"
 	"testing"
 
 	. "golang.org/x/tools/gopls/internal/test/integration"
@@ -48,6 +49,41 @@ baz` + "`" + `
 		}
 		if content != nil {
 			t.Errorf("hover expect empty result got: %v", content)
+		}
+	})
+}
+
+// TestIssue79146 verifies that hover information for a local variable does not
+// include any irrelevant comments.
+func TestIssue79146(t *testing.T) {
+	const src = `
+-- go.mod --
+module mod.com
+
+go 1.20
+
+-- main.go --
+package main
+
+// Foo comment
+func Foo() {
+  x := 1
+  _ = x
+}
+
+// F comment
+func F() {}
+`
+	Run(t, src, func(t *testing.T, env *Env) {
+		env.OpenFile("main.go")
+		env.Await(env.DoneWithOpen())
+		loc := env.RegexpSearch("main.go", "x")
+		content, _ := env.Hover(loc)
+		if content == nil {
+			t.Fatal("hover returned nil")
+		}
+		if strings.Contains(content.Value, "F") || strings.Contains(content.Value, "Foo") {
+			t.Errorf("hover result incorrectly includes irrelevant comment :\n%s", content.Value)
 		}
 	})
 }
