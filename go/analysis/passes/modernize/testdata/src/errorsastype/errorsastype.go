a132f2a97ee499ff94dda0bdd226436e4ece072a
Madeline Kalil <mkalil@google.com>
2026-05-19 10:39:38 -0400

    go/analysis/passes/modernize: errorsastype: support negated case
    Support an additional pattern:
    
    var myerr *MyErr
    if errors.As(err, &myerr) { ... }
    
    =>
    
    if myerr, ok := errors.AsType[*MyErr](err); !ok { ... }
    
    For golang/go#75692
    
    Change-Id: I3d9a378a4ea101f6e7d3e7c4a6e3beba417e3212
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/779800
    Reviewed-by: Alan Donovan <adonovan@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- go/analysis/passes/modernize/testdata/src/errorsastype/errorsastype.go
+++ go/analysis/passes/modernize/testdata/src/errorsastype/errorsastype.go
@@ -55,4 +55,34 @@ func _(err error) {
 			print(patherr, ok)
 		}
 	}
+	// Negated case.
+	{
+		var patherr *os.PathError
+		if !errors.As(err, &patherr) { // want `errors.As can be simplified using AsType\[\*os.PathError\]`
+			print(patherr)
+		}
+	}
+	{
+		var patherr *os.PathError
+		var linkerr *os.LinkError
+		if errors.As(err, &patherr) { // want `errors.As can be simplified using AsType\[\*os.PathError\]`
+			print(patherr)
+		} else if !errors.As(err, &linkerr) { // want `errors.As can be simplified using AsType\[\*os.LinkError\]`
+			print("not a use of linkerr")
+		}
+	}
+	{
+		var patherr *os.PathError
+		if !errors.As(err, &patherr) { // want `errors.As can be simplified using AsType\[\*os.PathError\]`
+			print("not a use of patherr")
+		} else {
+			print(patherr)
+		}
+	}
+	{
+		var patherr *os.PathError = &os.PathError{}
+		if !errors.As(err, &patherr) { // nope: would change the value of patherr observed by the print statement
+			print(patherr)
+		}
+	}
 }
