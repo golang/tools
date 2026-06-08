b9da64c8820114eb0869abbc11eec0ec31b7b063
Alan Donovan <adonovan@google.com>
2026-06-08 17:28:16 -0400

    go/types/typeutil: IntuitiveMethodSet: test with generic methods
    For golang/go#77549
    
    Change-Id: Iaba450abf40ed567880a3751741a5381ec4a2d6f
    Reviewed-on: https://go-review.googlesource.com/c/tools/+/788501
    Reviewed-by: Mark Freeman <markfreeman@google.com>
    LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

--- go/types/typeutil/ui_test.go
+++ go/types/typeutil/ui_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 
 	"golang.org/x/tools/go/types/typeutil"
+	"golang.org/x/tools/internal/testenv"
 )
 
 func TestIntuitiveMethodSet(t *testing.T) {
@@ -60,6 +61,56 @@ func (*A) g()
 		got := strings.Join(names, " ")
 		if got != test.want {
 			t.Errorf("IntuitiveMethodSet(%s) = %q, want %q", test.expr, got, test.want)
+		}
+	}
+
+	// variant with generic methods
+	if testenv.Go1Point() >= 27 {
+		const source = `
+package P
+type A int
+func (A) f()
+func (*A) g()
+func (*A) h[T any]() {}
+`
+
+		fset := token.NewFileSet()
+		f, err := parser.ParseFile(fset, "hello.go", source, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var conf types.Config
+		pkg, err := conf.Check("P", fset, []*ast.File{f}, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		qual := types.RelativeTo(pkg)
+
+		for _, test := range []struct {
+			expr string // type expression
+			want string // intuitive method set
+		}{
+			{"A", "(A).f (*A).g (*A).h"},
+			{"*A", "(*A).f (*A).g (*A).h"},
+			{"error", "(error).Error"},
+			{"*error", ""},
+			{"struct{A}", "(struct{A}).f (*struct{A}).g (*struct{A}).h"},
+			{"*struct{A}", "(*struct{A}).f (*struct{A}).g (*struct{A}).h"},
+		} {
+			tv, err := types.Eval(fset, pkg, 0, test.expr)
+			if err != nil {
+				t.Errorf("Eval(%s) failed: %v", test.expr, err)
+			}
+			var names []string
+			for _, m := range typeutil.IntuitiveMethodSet(tv.Type, nil) {
+				name := fmt.Sprintf("(%s).%s", types.TypeString(m.Recv(), qual), m.Obj().Name())
+				names = append(names, name)
+			}
+			got := strings.Join(names, " ")
+			if got != test.want {
+				t.Errorf("IntuitiveMethodSet(%s) = %q, want %q", test.expr, got, test.want)
+			}
 		}
 	}
 }
